4ed15a26dab30151ae9f6c5eb643bc130686175a
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-06 11:55:51 +0530

    Parent Child Releation ship

---
+++ basics-2/src/components/Child.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const Child = () => <p>Child Exporting in WelcomeClass.js . (For WelcomeClass this is a Child , For App WelcomeClass is a Child . For Index, app is a Child)</p>
