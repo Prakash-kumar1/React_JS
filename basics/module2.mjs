95b8e8f222d4eba4c9e97d3b3bf4834801149e86
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-06 15:00:57 +0530

    Exporting  and  Importing  Module

---
+++ basics/module2.mjs
@@ -0,0 +1,17 @@
+// To load an ES module, set "type": "module" in the package.json or use the .mjs extension.
+
+
+const a = "Prakash" ;
+const b = "Anjali" ;
+const c = "Ritesh" ;
+const d = "Govind" ;
+const e = "Rajesh" ;
+const f = "Shubhankar" ;
+const g = "Shivansh" ;
+
+
+export default c ;
+export {a} ;
+export {d} ;
+export {g} ;
+
