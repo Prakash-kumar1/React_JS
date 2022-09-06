9d0223901deb3d0125972569bb6b97f154ce083f
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-06 11:58:25 +0530

    Importing , Functional  and Class Based Components

--- basics-2/src/App.js
+++ basics-2/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { WelcomeClass } from './components/WelcomeClass';
 
-function App() {
+// // importing welcome function from components
+import {WelcomeFn} from "./components/WelcomeFn" ;
+
+
+// function App() {
+// // Remove Everything In-Built and atarted from scratch .
+//   return ( 
+//     <div>
+//   <h1>Welcome to React class</h1>
+//   <p>If we can,t wrap h1 and p inside div  Or  Section tag , it will throw Error because there are two elements</p>
+//   </div>
+//   );  
+// }
+
+// // Here I am Exporting by default , so, we change file name in Index.js from App to  Prakash or any Name.
+// export default App;
+
+
+// Using Functions for exporting (comment Line no. 4 to line no. 15)
+
+export function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section>
+      <h1>Using Function For exporting</h1>
+      <p>This is a function and there is no need of export default app</p>
+      <WelcomeFn/>
+
+{/* each one of will come 3 times because we copy App 3 times */}
+      <WelcomeClass name="Sameer" age= "20" />
+      <WelcomeClass name="Anjali" age= "23" />
+      <WelcomeClass name="Prakash" age= "22" />
+      <WelcomeClass name="Govind" age= "21" />
+      
+    </section>
+  ) ;
 }
 
-export default App;
+// Never use Two function in a js File 
+export function App2(){
+  return <h2>app2 is a second function </h2>
+}
+
