a12034ccfe47e228621927d6d5b64ddf5a70825d
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-06 09:53:46 +0530

    Basic  Concepts  of  React-Js

--- basics/src/App.js
+++ basics/src/App.js
@@ -7,16 +7,16 @@ let name = "kumar-(Frontend Engineer)" ;          //Always declaring above
 
 function App() {
   return (
+ 
 
 // using <> and </> , when we write something above on  div .
     <>
-
     <h1> This is me , Prakash {name} </h1>
     <p>React - JS is a Client side javascript library . ( HTML + JS ==  JSX) it is used to build User Interface (UI designs) and Single Page Applications (SPA) . Loading the page is called " Rendering " . react - Js is lightWeight , Fastest and React has Component based Architecture . React Js follow  Declartive approach (line no.- 4) . React-js creates Virtual DOM . compare to Real DOM , Virtual DOM is very lightWeight and fast . it is like internal tree data structure . </p>
     
     <div className="ist">Lovely</div>
     <img src="C:\Users\Monika Priya Singh\OneDrive\Pictures\Screenshots\github.jpg" alt="nothing Image found" />
-
+   
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -29,12 +29,13 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Prakash 
+          Learn React with Prakash Kumar
         </a>
       </header>
     </div>
     
     </>
+   
   );
 }
 
