a4009a1016ca177b3489a0aefdcb29527f0fbb6d
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-07 11:42:58 +0530

    Like  Object  Destructuring  Properties

--- basics-2/src/components/WelcomeClass.js
+++ basics-2/src/components/WelcomeClass.js
@@ -5,7 +5,7 @@ import { Child } from "./Child";
 
 export class WelcomeClass extends Component {
     render() {
-        const {name,age} = this.props
+        const {name,age,place,food} = this.props
 
 // // props  is  Property short form . props are read only data means , we can't change the data .
 //         console.log(this.props) ;
@@ -14,11 +14,12 @@ export class WelcomeClass extends Component {
             <div>
                 <Child />
         <h2>
-            Welcome to Aircampus {this.props.name} . My age is {this.props.age} This is a class based components.
+
+            {/* Welcome to Aircampus {this.props.name} . My age is {this.props.age} This is a class based components..i am from {this.props.place} */}
 
     {/* OR we can write also this way */}
 
-            Welcome to Aircampus {name} . My age is {age} This is a class based components.
+            Welcome to Aircampus {name} . My age is {age} .i am from {place} and i love to eat {food} .
             </h2>
             </div>
             ) ;
