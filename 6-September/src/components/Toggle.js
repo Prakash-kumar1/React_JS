1494452462c1574e7ac37505405d3111ef706ab4
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-07 18:19:22 +0530

    Toggle  Created

--- 6-September/src/components/Toggle.js
+++ 6-September/src/components/Toggle.js
@@ -1,33 +1,43 @@
-import React , {Component} from "react" ;
+import React, { Component } from "react";
 
-export class Toggle extends Component{
-
-// state ==>>  store some data inside a variable
-
-       constructor(props){
-        super(props) ;
-    // state values are changable 
+export class Toggle extends Component {
+  // state -> store some data inside a variable
+  constructor(props) {
+    super(props);
+    // state values are changable
     this.state = {
-        isToggleOn : false ,
-    } ;
+      isToggleOn: false,
+    };
 
-    this.handleClick = this.handleClick.bind(this)
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (click) => {
+    console.log(this);
+    console.log(click);
+    this.setState({
+      isToggleOn: !this.state.isToggleOn,
+    });
+  };
+
+  render() {
+    return (
+      //   <button
+      //     onClick={() =>
+      //       this.handleClick({
+      //         name: "kuldeep",
+      //       })
+      //     }
+      //   >
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? "ON" : "OFF"}
+      </button>
+    );
+  }
 }
 
-       handleClick(){
-        console.log(this);
-        this.state.isToggleOn = true ;
-       }
 
-    render(){
-        return( 
-            <button onClick={this.handleClick}>
-            {this.state.isToggleOn ? "ON" : "OFF"}
-            </button>
-        )
-    }
-}
-   
-    
+
+
 
 
