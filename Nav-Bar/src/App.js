46134a1aee4f1d3a26aa1c08bcc2cdc0097aaebd
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-08 18:05:46 +0530

    Project  Progressing

--- Nav-Bar/src/App.js
+++ Nav-Bar/src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 
 function App() {
@@ -10,6 +11,14 @@ function App() {
   <Navbar/>
 {/* passing tile in navbar*/}
   <Navbar title = "Project-1 Text-Utils" aboutText = "about Project-1" />
+
+{/* textForm taking whole width , not look good. for solving this , we use a bootstrap class which is Container and putting textForm in a div (try without div , then know the difference) */}
+
+<div className="container">
+{/* passing heading as a Properties */}
+  <TextForm heading="Enter your text to analyze below" />
+  </div>
+
 </>
 
   );
