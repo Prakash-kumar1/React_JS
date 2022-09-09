052750f04935e1bd24611509d2899e85bec6aaa3
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-09 18:57:24 +0530

    jsx ( HTML + JS) File    for a Button

--- 8-September/src/components/Button.jsx
+++ 8-September/src/components/Button.jsx
@@ -3,4 +3,10 @@
 // you can also use  js
 
 
+import React, { Component } from "react";
 
+export class Button extends Component {
+  render() {
+    return <button className={this.props.class}>{this.props.text}</button>;
+  }
+}
