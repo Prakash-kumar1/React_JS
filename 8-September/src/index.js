4bf9cf5de65fa6c53b52930416280236e9da8001
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-09 17:51:28 +0530

    React DOM  Information

--- 8-September/src/index.js
+++ 8-September/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';     //ReactDOM is a class  present in  NPM  package.
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render(        //render means , create element in the UI
   <React.StrictMode>
     <App />
   </React.StrictMode>
