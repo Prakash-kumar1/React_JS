431f01150bbafc8cf2773b5fcd4d214fb8c15450
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-09 20:21:21 +0530

    Starting  9 September  Sessions

---
+++ 9-September/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+
+let name = "kumar-(Frontend Engineer)" ;          //Always declaring above 
+
+// concept of getting data without reloading (Refreashing) the page , is known as  AJAX .
+
+function App() {
+  return (
+ 
+
+// using <> and </> , when we write something above on  div .
+    <>
+    <h1> This is me , Prakash {name} </h1>
+    <p>React - JS is a Client side javascript library . ( HTML + JS ==  JSX) it is used to build User Interface (UI designs) and Single Page Applications (SPA) . Loading the page is called " Rendering " . react - Js is lightWeight , Fastest and React has Component based Architecture . React Js follow  Declartive approach (line no.- 4) . React-js creates Virtual DOM . compare to Real DOM , Virtual DOM is very lightWeight and fast . it is like internal tree data structure . </p>
+    
+    <div className="ist">Lovely</div>
+    <img src="C:\Users\Monika Priya Singh\OneDrive\Pictures\Screenshots\github.jpg" alt="nothing Image found" />
+   
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          Today we are starting React with Vasanth sir .
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React with Prakash Kumar
+        </a>
+      </header>
+    </div>
+    
+    </>
+   
+  );
+}
+
+export default App;
