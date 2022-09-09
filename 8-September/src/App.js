7528932ca92e8517766eedf39182a762d67edd3d
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-09 18:08:27 +0530

    hj

--- 8-September/src/App.js
+++ 8-September/src/App.js
@@ -2,9 +2,16 @@
 
 import React, {Component} from "react" ;
 
-class App extends Component{    //extends taking property of parents , read in Inheritance
+export class App extends Component{    //extends taking property of parents , read in Inheritance
   render(){
-    return
+    return(
+     <div>
+      <h1>Title : Harry Potter and the blood of prince</h1>
+      <p>Hello World</p>
+     </div>
+    );
+    
 
   }
 }
+// export default App  (using export above , so putting app in { } in index.js)
--- 8-September/src/index.js
+++ 8-September/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';     //ReactDOM is a class  present in  NPM  package.
 import './index.css';
-import App from './App';
+import {App} from './App';      //this method is using when export with class
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
