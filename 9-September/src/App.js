87b0edcc1168e8fa544fade85c51112efb8dfbff
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-12 19:56:34 +0530

    Update App.js

--- 9-September/src/App.js
+++ 9-September/src/App.js
@@ -5,14 +5,24 @@ import "./App.css" ;
 
 
 export class App extends Component{    //extends taking property of parents , read in Inheritance
+  constructor(props){
+    super(props) ;
+    this.state = {
+      text : "Delete" ,
+    };
+  }
+
+
   render(){
-    console.log(this) ;
+    // console.log(this) ;
     return(
 
      <div>
       <h1>Title : {this.props.name} and the blood of prince</h1>
       <p>Hello World , </p>
-      <Button text="Delete" btnClass="delete" />
+      <Button text={ this.state.text} btnClass="delete" />
+{/*Comment line no 8 to 13 and 23 then use line no. 25 , both are same . */}
+      {/* <Button text="Delete" btnClass="delete" /> */}
         <Button text="Save" btnClass="save" />
      </div>
      
