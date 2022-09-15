a784bc7f2a15ebc60229faeeb531ac5165ce0051
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-15 17:39:56 +0530

    Update Main.jsx

---
+++ 14=September/src/Main.jsx
@@ -0,0 +1,11 @@
+// Component  is  a  In-Built  class 
+
+import React, {Component} from "react" ;
+
+
+ class Main extends Component{    
+    constructor(props){
+      super(props) ;
+      ;
+    }
+}
