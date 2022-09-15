170763c3c37cfa44ae2ad3648dc1cdf55300d200
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-15 18:09:59 +0530

    Update Main.jsx

--- 14=September/src/Main.jsx
+++ 14=September/src/Main.jsx
@@ -13,7 +13,8 @@ export class Main extends Component{
 
     render(){
         return (
-// In the place od div , we can use this <> and </>
+// In the place od div , we can use this <> and </> (internally it calls React.fragment)
+
             <>
         <h1>This is my Parent Component . </h1>
         <Child />
