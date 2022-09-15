0ef39762c664adbac7a49f390e7ba9d7b46f84c6
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-15 18:04:17 +0530

    Parent and Child Component

--- 14=September/src/Main.jsx
+++ 14=September/src/Main.jsx
@@ -1,6 +1,7 @@
 // Component is a  In-Built  class which have many methods
 
 import React, {Component} from "react" ;
+import { Child } from "./components/Child"
 
 // if i extends this component then Main become a component . otherwise , Main is just a normal function .
 
@@ -11,7 +12,12 @@ export class Main extends Component{
     }
 
     render(){
-        return <h1>This is my Parent Component . </h1> ;
+        return (
+            <div>
+        <h1>This is my Parent Component . </h1>
+        <Child />
+        </div>
+        ) ;
     }
 }
 
--- 14=September/src/components/Child.jsx
+++ 14=September/src/components/Child.jsx
@@ -1,1 +1,31 @@
-import React, {Component} from "react" ;
+import React, { Component } from "react" ;
+
+
+export class Child extends Component{
+    constructor(props){
+        super(props) ;
+    }
+
+    render(){
+        return <h2>This is my Child Component .</h2>
+
+    }
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
