6fc28d4b779031b281eac007aae1d16523c42941
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-15 16:50:00 +0530

    Update App.js

--- 14=September/src/App.js
+++ 14=September/src/App.js
@@ -25,12 +25,12 @@ export class App extends Component{    //extends taking property of parents , re
      <div>
       <h1>Title : {this.props.name} and the blood of prince</h1>
       <p>Hello World , </p>
-      <Button text={ this.state.text} handleBtnApp ={this.handleBtnApp} btnClass="delete" />
+      {/* <Button text={ this.state.text} handleBtnApp ={this.handleBtnApp} btnClass="delete" /> */}
 
 {/*Comment line no 8 to 13 and 23 then use line no. 25 , both are same . */}
 
       {/* <Button text="Delete" btnClass="delete" /> */}
-        <Button text="Save" btnClass="save" />
+        {/* <Button text="Save" btnClass="save" /> */}
      </div>
      
     ); 
