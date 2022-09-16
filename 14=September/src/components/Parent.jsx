9f2a532f1d5ba011d7dba3e2f71d641e032302f1
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-16 23:02:13 +0530

    Parent Properties , Increasing and Decreasing , passing Props

--- 14=September/src/components/Parent.jsx
+++ 14=September/src/components/Parent.jsx
@@ -11,12 +11,19 @@ export class Parent extends Component{
  // Intialize  the  state  here 
     this.state = {
         parentText : "Default Parent state" ,
+        parentObj : {
+            name : "Prakash kumar",
+            food : "Dosa" ,
+            work : "Fronted Developer" ,
+        },
+        count1 : 0 ,
+        count2 : 0 ,
     } ;
  }
 
    changeParentText = () => {   //this is a  function which change parents property
     this.setState({
-        parentText : "CHANGED " ,
+        parentText : "CHANGED " ,    
     }) ;
    } ;
 
@@ -29,6 +36,23 @@ export class Parent extends Component{
     }) ;
    } ;
 
+
+// Increasing Count value 
+changeCount = () =>{
+    this.setState((prev) => ({
+        count1 : prev.count1 +1 ,
+    }));
+}
+
+// Decreasing Count value 
+decreaseCount = () =>{
+    this.setState((prev) => ({
+        count2 : prev.count2 -1 ,
+    }));
+}
+
+
+
     render(){
         console.log("parent") ;  //if i changed my parent component (clicking on parent button)
 // all my child will Re-rendered .(see in console)
@@ -39,9 +63,14 @@ export class Parent extends Component{
             <>
         <h1>This is my Parent Component = {this.state.parentText} </h1>
         <button onClick={this.changeParentText}>Parent</button>
+        <button onClick={this.changeCount} > Increment = {this.state.count1}   </button>
+        <button onClick={this.decreaseCount} > Decrement = {this.state.count2} </button>
         <Child />
 {/* passing function itself and got into console */}
-        <Child changeByChild={this.changeByChild}/>
+        <Child  
+         changeByChild={this.changeByChild} 
+          lightObj = {this.state.parentObj}       //not working , don't know reason
+          />
         </>
 
         ) ;
