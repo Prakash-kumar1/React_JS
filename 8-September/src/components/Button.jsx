678082eed07b8c9df04541f58b6887f87dd07e3e
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-16 11:22:45 +0530

    Class Component  Definition

--- 8-September/src/components/Button.jsx
+++ 8-September/src/components/Button.jsx
@@ -1,7 +1,9 @@
 // .jsx  is  only  used  for  Class Based  Componenets  . never used for Function Based  Componenets
-// .jsx gives facility like HTML 
-// you can also use  js
+// .jsx gives facility like HTML (self closed tag , attributes),  you can also use  js . 
 
+// Class Component :=>  When a component start from class keyword is known as Class Component . we are using
+//                 " state " only inside the Class Component . that's why it is also known as "Stateful Component"
+//                 For showing the data we used Class Component  and for modifying the data we used Functional Component  
 
 import React, { Component } from "react";
 
