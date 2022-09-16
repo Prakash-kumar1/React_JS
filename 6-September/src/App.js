a2bde4cac8c90a2f027ac3149b52da032be81d55
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-16 13:15:49 +0530

    Using  Props in Components

--- 6-September/src/App.js
+++ 6-September/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import {Toggle} from './components/Toggle.js'
 // inside curly braces toggle represents class
 
+import Prop from './Prop';
+
 
 
 
@@ -10,7 +12,17 @@ function App() {
   return (
     <section>
       <Toggle name="Present in props , console" />
-    
+<Prop 
+
+// Passing Props in Functional component
+text="Passing data in props" 
+obj={{name:'Rinki' , food:'Dosa' , note: 'Passing Object in two curly Braces'}}
+
+// Passing Props in Class component
+text1="Passing data in class component always used with this keyword"
+obj1={{name:'Prakash' , food:'Chicken' , note: 'Passing Object in two curly Braces'}}
+/>
+
     </section>
   );
 }
