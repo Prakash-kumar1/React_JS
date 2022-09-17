b0a1ab62b8a68a3d76954ed0fb16dc41b237d8a3
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-17 13:37:49 +0530

    16=> September

--- 15-September/src/App.js
+++ 15-September/src/App.js
@@ -1,12 +1,40 @@
+import React from "react";
+import Person from './Components/Person';
+import Person2 from './Components/Person2';
 
-import './App.css';
 
+ export class App extends React.Component{
+  constructor(){
+    super () 
+    this.state ={
+       count :0,
+       max:100,
+       min:-10 , 
+    }
+  }
 
-function App() {
-  return (
-   
-  );
+  increment =()=>{ 
+    this.setState(this.state.count<this.state.max ?{count:this.state.count+1}:{count:this.state.count})
+  }
+
+  decrement =()=>{
+    this.setState(this.state.count<=this.state.min ?{count:this.state.count}:{count:this.state.count-1})
+  }
+
+ render()
+ {
+  return(
+    <div>
+      <h1 className="count">{this.state.count}</h1>
+      <button onClick={this.increment}>Add</button>
+      <button onClick={this.decrement}>Substract </button>
+
+{
+  this.state.count > 5 ? <Person count={this.state.count} name = "Prakash Kumar" /> : <Person2/>
 }
+    </div>
+  )
+ }
 
-export default App;
+}
 
