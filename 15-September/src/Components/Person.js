4ef1e2edbb3a58ffe084de6ea18011c216ce9984
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-17 19:59:51 +0530

    Passing Function as a Props  and   React-Bootstarp

--- 15-September/src/Components/Person.js
+++ 15-September/src/Components/Person.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
-
+import Button from 'react-bootstrap/Button';
 
 export default class Person extends Component {
   render() {
     let {name , count , increase} = this.props
     return (
       <div>
-        <button onClick={increase}>Plus</button>
+        {/* <button onClick={increase} >Plus</button> */}
+
+        {/* Using React-Bootstarp */}
+        <Button onClick={increase}   variant="success" >Plus</Button>
+            
         Hello Everyone Person , {name} = {count}
     </div>
     )
