7d923f4a4c2c5b49fee9d9ac2746d94b3d417b10
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-17 21:06:06 +0530

    Destructruning  in Person2.js

--- 15-September/src/Components/Person2.js
+++ 15-September/src/Components/Person2.js
@@ -5,8 +5,10 @@ export default class Person2 extends Component {
     let {name , count , decrease} = this.props
     return (
       <div>
+
         <button onClick={decrease}>minus </button>
         Hi , I am Person2 with {name} and {count}
+        
       </div>
     )
   }
