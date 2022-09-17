690c108aeaea3d608f015ed27fb900201bba47b0
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-17 22:05:14 +0530

    Update Greeting.jsx

--- 14=September/src/components/Greeting.jsx
+++ 14=September/src/components/Greeting.jsx
@@ -10,7 +10,14 @@ export class Greeting extends Component {
 
 
   render() {
-    <p>Please Log in</p>
-    return <p>Welcome Govind</p>
+    if( this.state.isLoggedIn){
+        return  <p>Welcome Govind</p> 
+    }else{
+        return <p>Please Log in</p> 
+    }
+
+    // OR We can also use  Ternariy  Operator
+    
+    // return this.state.isLoggedIn ?  <p>Welcome Govind</p> : <p>Please Log in</p> ;
   }
 }
