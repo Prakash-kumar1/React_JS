af4ee7f5e7e6c066086bdb8444193f94b06471db
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-17 23:34:06 +0530

    Using if-Else , Ternary , Variables , Function Methods

--- 14=September/src/components/Greeting.jsx
+++ 14=September/src/components/Greeting.jsx
@@ -1,16 +1,60 @@
 import React, { Component } from 'react'
 
+// Learning 3 methods in 16-September session
+// 1==>  Using if-Else inside render() but writting same code again .
+// 2==>  Using  Ternary Operator inside render ()
+// 3==>  Using  Variables  inside render ()
+// 4==>  Using  Functions (writting all conditions inside the function ) outside render ()
+
+
 export class Greeting extends Component {
     constructor(props){
         super(props) ;
         this.state = {
             isLoggedIn: false ,
             message : true ,
+            chats : true ,
         }
     }
 
+// Using Functions ==>>
+//     getLoveMessage = () =>{
+//         let loveMessage;
+//     if(this.state.chats){
+//         loveMessage = (
+//         <>
+//         <p>I Love You </p>
+//         <p>From Bottom of my Heart</p> ;
+//         </>
+//         );
+//     }else{
+//         loveMessage = <p>I hate you</p> ;
+//     }
+//     return loveMessage ;
+//     };
+
+
+// // Optimizing the above Function Code :=> we write By default Line no.-41 is my condition but if Below
+// "If condition" is true then print Line no.-46 ,47. this is also a way to write code for avoiding Else block .
+
+
+    getLoveMessage = () =>{
+        let loveMessage= <p>I hate you</p>;
+    if(this.state.chats){
+        loveMessage = (
+        <>
+        <p>I Love You </p>
+        <p>From Bottom of my Heart</p> ;
+        </>
+        );
+    }
+    return loveMessage ;
+    };
+
 
   render() {
+
+// ##### ==>>When checking If Else conditions comment welcomeMessage and  Ternariy  Operator  whole code <<==#####
 
     // if( this.state.isLoggedIn)  //{
     // return (
@@ -50,6 +94,10 @@ export class Greeting extends Component {
     {/* OR passing value through variable*/}
 
     {welcomeMessage}
+
+    {/* OR passing value through Functions*/}
+
+    {this.getLoveMessage()}
     </>
     ) ;
   }
