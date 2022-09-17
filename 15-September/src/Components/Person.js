6bd86844332e7b65bdedad866b6461c22d8374d2
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-17 21:05:09 +0530

    Normal  and  React-Bootstarp Button

--- 15-September/src/Components/Person.js
+++ 15-September/src/Components/Person.js
@@ -6,11 +6,12 @@ export default class Person extends Component {
     let {name , count , increase} = this.props
     return (
       <div>
+        
         {/* <button onClick={increase} >Plus</button> */}
 
         {/* Using React-Bootstarp */}
         <Button onClick={increase}   variant="success" >Plus</Button>
-            
+
         Hello Everyone Person , {name} = {count}
     </div>
     )
