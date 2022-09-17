913d4156c595de121dbeef618c9d1b991aa6e76b
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-17 17:14:36 +0530

    Passing Props in Person2

--- 15-September/src/Components/Person2.js
+++ 15-September/src/Components/Person2.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react'
 
 export default class Person2 extends Component {
   render() {
+    let {name , count} = this.props
     return (
-      <div>Hi , I am Person2 </div>
+      <div>Hi , I am Person2 with {name} and {count}</div>
     )
   }
 }
