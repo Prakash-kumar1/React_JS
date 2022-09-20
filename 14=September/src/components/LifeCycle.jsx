b64e8e3c77c7d909fbb06b93b6ea4395c5cc0667
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-20 19:02:14 +0530

    Update LifeCycle.jsx

--- 14=September/src/components/LifeCycle.jsx
+++ 14=September/src/components/LifeCycle.jsx
@@ -1,3 +1,5 @@
+////@@@###$$$%%%***  19-September sessions   //@@@###$$$%%%***
+
 
 import React , {Component}from "react";
 
@@ -7,6 +9,7 @@ export class LifeCycle extends Component{
     this.state = {
         count : 1,
     };
+    console.log("Intialization") ;
    } 
 
 
@@ -16,13 +19,36 @@ export class LifeCycle extends Component{
     })) ;
    }
 
+//    Life cycles methods =>>
+componentDidMount(){
+    console.log("component Did Mount") ;
+}
+
+componentWillMount(){
+    console.log("component Will Mount") ;
+}
+
+shouldComponentUpdate(){
+    console.log("should component Update") ;
+// if return is false then value not increases , you can check
+    return true ;
+}
+
    render(){
+    console.log("Rendering") ;
     return(
         <>
         <p>count = {this.state.count}</p>
         <button onClick={this.incrementCount}>increment</button>
-        {this.state.count > 5 && <p>Hello Prakash </p>}
+        {this.state.count > 5 && <h1>Hello Prakash </h1>}
         </>
     ) ;
    }
 }
+
+////@@@###$$$%%%***  19-September sessions , Sir Disscuss the Life-Cycle method Please watch Once more .   //@@@###$$$%%%***
+// There are 4 phases in Life Cycle method (for detail , see 19-September sessions)
+// (1) =>  Intialization
+// (2) =>  Mounting
+// (3) =>  Updation
+// (4) =>  Un-Mounting
