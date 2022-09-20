950e57572b8cd0974e2069f300a479018d1f3c27
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-20 18:37:32 +0530

    Completeing Life cycle method

--- 14=September/src/components/LifeCycle.jsx
+++ 14=September/src/components/LifeCycle.jsx
@@ -1,1 +1,28 @@
-import React from "react";
+
+import React , {Component}from "react";
+
+export class LifeCycle extends Component{
+   constructor(props){
+    super(props);
+    this.state = {
+        count : 1,
+    };
+   } 
+
+
+   incrementCount = () =>{
+    this.setState((prevState) => ({
+        count : prevState.count + 1 ,
+    })) ;
+   }
+
+   render(){
+    return(
+        <>
+        <p>count = {this.state.count}</p>
+        <button onClick={this.incrementCount}>increment</button>
+        {this.state.count > 5 && <p>Hello Prakash </p>}
+        </>
+    ) ;
+   }
+}
