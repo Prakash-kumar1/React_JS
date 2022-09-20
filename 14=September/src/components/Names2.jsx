0090b8daf2f26d91640f217245a0e4e2a21b773e
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-20 18:22:25 +0530

    Using Index-Value , ID's ,  Childrens

--- 14=September/src/components/Names2.jsx
+++ 14=September/src/components/Names2.jsx
@@ -6,6 +6,7 @@ export default class Names2 extends Component {
   constructor(props){
     super(props)
     this.state = {
+      breeds : ['Golden Retriver' , 'German Sheperd' , 'Lebrador' , 'Bulldog' , 'Bulldog' , 'Pamerian'],
       names : [
         {
             id : 1 ,
@@ -39,7 +40,14 @@ export default class Names2 extends Component {
 
     <>
 
-    {this.props.children}
+{this.props.children}
+
+{/* There is two Bulldog , so, if we delete one Bulldog another also delete . so, we use Index */}
+{this.state.breeds.map((dog , index) =><p key={index}>{dog}</p>)} 
+
+
+{/* But there is one more problem , if i delete Lebrador [index value = 2] then every index value fill that place 
+then Bulldog have index value = 2 . so, for removing this problem we use ID */}
 
     {this.state.names.map((nameObj) => (
     <p key={nameObj.id}>
