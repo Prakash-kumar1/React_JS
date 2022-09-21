d1bb1068c9b6b5370db0ceae16ed9f0a5d51ecc9
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-21 11:43:21 +0530

    Update Parent.jsx

--- 15-September/src/Components/Parent.jsx
+++ 15-September/src/Components/Parent.jsx
@@ -1,14 +1,32 @@
-import React, { Component } from 'react'
+// @@@###$$$%%%***  Starting  20-September sessions  @@@###$$$%%%***
 
-export class Parent extends Component {
-  render() {
-    return (
-      <>
-      <h1>Header = 1</h1>  
-      <p> Paragraph</p>
-      <h2>Header = 2</h2>  
-      </>
+
+// import React, { Component } from 'react'
+// export class Parent extends Component {
+//   render() {
+//     return (
+//       <>
+//       <h1>Header = 1</h1>  
+//       <p> Paragraph</p>
+//       <h2>Header = 2</h2>  
+//       </>
       
-    )
-  }
-}
+//     )
+//   }
+// }
+
+
+// In above , I am not writting any state . so, we can use function.
+// This is called Stateless Functional Component
+
+export function Parent() {
+
+      return (
+        <>
+        <h1> Without state</h1>  
+        <p> I am using Function</p>
+        <h2>Stateless Functional Component</h2>  
+        </>
+        
+      )
+    }
