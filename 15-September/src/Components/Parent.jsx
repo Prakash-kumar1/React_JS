901d32b09bfe9bcb1d4bf2ec8c583c6844d46ed9
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-21 13:44:09 +0530

    Class , Stateless Functional , Importing Pure and Regular Component

--- 15-September/src/Components/Parent.jsx
+++ 15-September/src/Components/Parent.jsx
@@ -17,18 +17,59 @@
 // }
 
 
-// In above , I am not writting any state . so, we can use function.
-// This is called Stateless Functional Component
+// // In above , I am not writting any state . so, we can use function.
+// // This is called Stateless Functional Component
 
-export function Parent(props) {
+// export function Parent(props) {
 
-      return (
-        <>
-        <h1> Without state {props.text}</h1>  
-        <p> I am using Function with the name is {props.name[1]}</p>
-        <p> My name is {props.name[2]}</p>
-        <h2>Stateless Functional Component</h2>  
-        </>
+//       return (
+//         <>
+//         <h1> Without state {props.text}</h1>  
+//         <p> I am using Function with the name is {props.name[1]}</p>
+//         <p> My name is {props.name[2]}</p>
+//         <h2>Stateless Functional Component</h2>  
+//         </>
         
-      );
-    }
+//       );
+//     }
+
+
+
+// ##### (Pure Component && Regular Component) Using in Parent.jsx ####
+
+import React, { Component } from 'react'
+import Regular from './Regular';
+import { Pure } from './Pure';
+
+export class Parent extends Component {
+  constructor(props){
+    super(props) ;
+    this.state = {
+      name : "amit" ,
+    } ;
+  }
+
+  render() {
+    console.log("----> Parent Component <----")
+
+    return (
+      <>
+
+       <p>Inside Parent Component Importing</p>
+       <Regular name = {this.state.name}/>
+       <Pure   name = {this.state.name}/>
+       <button onClick={ () => this.setState({ name : "Prakash"})}> Change Name</button>
+       <button onClick={ () => this.setState({ name : "anjali"})}> Change Name</button>
+
+{/* forceUpdate() ==> Forcefully render data on my page even though my State and Props are not changes */}
+  {/* It is in-Built method . Try to never used forceUpdate()  */}
+      <button onClick={ () => this.forceUpdate() }>force Update</button>
+
+        </>
+    )
+  }
+}
+
+
+
+
