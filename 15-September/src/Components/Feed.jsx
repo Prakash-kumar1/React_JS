a7e39cc68bbe72fccc798120f3f56a07c1e43c62
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-21 12:30:05 +0530

    Instagram UI  using  stateless functional component

---
+++ 15-September/src/Components/Feed.jsx
@@ -0,0 +1,26 @@
+import "./feed.css";
+
+// stateless functional component :  Props are allowed , state is not allowed
+
+export function Feed(props) {
+  console.log(props);
+  return (
+    <>
+      <div key={props.feed.id}>
+        <div className="myUserName">
+          <img className="profileImage" src={props.feed.imgUrl} alt="image" />
+          <div className="headingText">{props.feed.userName}</div>
+        </div>
+        <div>{props.feed.msg}</div>
+        <img className="contentImage" src={props.feed.imgUrl} alt="image" />
+        <br />
+        <i className="fa-regular fa-heart myFonts"></i>
+        <i className="fa-regular fa-comment myFonts"></i>
+        <i className="fa-regular fa-paper-plane myFonts"></i>
+        <br />
+        <br />
+        <br />
+      </div>
+    </>
+  );
+}
