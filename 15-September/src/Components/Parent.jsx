56bb19a667a09e430a5d4324c4664b04a6fba680
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-21 11:38:28 +0530

    Starting  20- September sessions

---
+++ 15-September/src/Components/Parent.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Parent extends Component {
+  render() {
+    return (
+      <>
+      <h1>Header = 1</h1>  
+      <p> Paragraph</p>
+      <h2>Header = 2</h2>  
+      </>
+      
+    )
+  }
+}
