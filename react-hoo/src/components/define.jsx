a8a251ade1536aba4b87f7cd0b6a9015f5e56038
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-22 20:54:29 +0530

    Update define.jsx

--- react-hoo/src/components/define.jsx
+++ react-hoo/src/components/define.jsx
@@ -4,10 +4,16 @@
 //                     few  built-in  Hooks like  useState ,  useEffect  etc . Hooks are new addition in React 16.8 . 
 
 
-// when use Hooks ==>>  If you write a function component and realize you need to add some state to it .
+// when use Hooks ==>>  If you write a function component and realize you need to add some state to it . then use hooks .
 
 
-
+// Rules of  Hooks ==>> 
+// (1) =>   Only call  Hooks  at the top level - We should not call hooks inside loops , Conditions or  Nested
+//           Functions . Instead always use hooks at the top level of your React Functions .
+ 
+// (2) =>   Only call Hooks  from React Functions - We should not call hooks from regular Javascript Functions . 
+// Instead call hooks from  React Function components  OR  call hooks from  Custom Hooks .
+ 
 
 
 
