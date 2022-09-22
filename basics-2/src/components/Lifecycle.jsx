d46eaabec76eab0f20b6781680d45cd5250422ef
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-22 16:53:02 +0530

    Update Lifecycle.jsx

--- basics-2/src/components/Lifecycle.jsx
+++ basics-2/src/components/Lifecycle.jsx
@@ -1,1 +1,7 @@
 // Each component has several "Life Cycle Methods" that you can override to run code at particular times in Process. 
+// There are  4 phases in  Life Cycle Methods  =>>
+// (1) =>  Intialization 
+// (2) =>  Mounting 
+// (3) =>  Updating 
+// ( ) =>  Error handling  (not count in Life Cycle phase)
+// (4) =>  Un-Mounting 
