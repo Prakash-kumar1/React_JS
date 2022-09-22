596b38167979b0ce36e537f12eae64db4ac1c58b
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-22 20:46:23 +0530

    Update define.jsx

--- react-hoo/src/components/define.jsx
+++ react-hoo/src/components/define.jsx
@@ -1,4 +1,27 @@
 // Hooks Definition =>>  Hooks  are  functions that let you "Hook into"  React state  and life-cycle features from 
 //                        Function components.  hooks  allows  you to use  React  without classes . it means you
-//                        can  use state  and other  React features  without  writting  a class . React provides a 
-//                        few  built-in  Hooks like  useState ,  useEffect  etc . 
+//                     can  use state  and other  React features  without  writting  a class . React provides a 
+//                     few  built-in  Hooks like  useState ,  useEffect  etc . Hooks are new addition in React 16.8 . 
+
+
+// when use Hooks ==>>  If you write a function component and realize you need to add some state to it .
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
