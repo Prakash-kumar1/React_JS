800b008fa9dd61b5aa7be4eef2f0e5f4ecf710bc
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-22 17:00:05 +0530

    Update Lifecycle.jsx

--- basics-2/src/components/Lifecycle.jsx
+++ basics-2/src/components/Lifecycle.jsx
@@ -3,11 +3,16 @@
 // (1) =>  Intialization 
 // (2) =>  Mounting 
 // (3) =>  Updating 
-// ( ) =>  Error handling  (not count in Life Cycle phase)
 // (4) =>  Un-Mounting 
+// ( ) =>  Error handling  (not count in Life Cycle phase)
 
 
+// (1) =>  Intialization =>  It is the birth phase of the lifecycle of a ReactJS component. Here, the component starts its journey on a way to the DOM. In this phase, a component contains the default Props and initial State. These default properties are done in the constructor of a component. The initial phase only occurs once and consists of the following methods.
 
+// getDefaultProps()
+// It is used to specify the default value of this.props. It is invoked before the creation of the component or any props from the parent is passed into it.
+// getInitialState()
+// It is used to specify the default value of this.state. It is invoked before the creation of the component.
 
 
 
