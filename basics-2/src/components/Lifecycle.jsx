1ca965a0bbe94b3ff2cd8cad5ffdd778dd791394
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-22 17:07:34 +0530

    Update Lifecycle.jsx

--- basics-2/src/components/Lifecycle.jsx
+++ basics-2/src/components/Lifecycle.jsx
@@ -25,10 +25,23 @@
 
 
 
+// (3) =>  Updating Phase => It is the next phase of the lifecycle of a react component. Here, we get new Props and change State. This phase also allows to handle user interaction and provide communication with the components hierarchy. The main aim of this phase is to ensure that the component is displaying the latest version of itself. Unlike the Birth or Death phase, this phase repeats again and again. This phase consists of the following methods.
+
+// componentWillRecieveProps() => It is invoked when a component receives new props. If you want to update the state in response to prop changes, you should compare this.props and nextProps to perform state transition by using this.setState() method.
+
+// shouldComponentUpdate() => It is invoked when a component decides any changes/updation to the DOM. It allows you to control the component's behavior of updating itself. If this method returns true, the component will update. Otherwise, the component will skip the updating.
+
+// componentWillUpdate() =>  It is invoked just before the component updating occurs. Here, you can't change the component state by invoking this.setState() method. It will not be called, if shouldComponentUpdate() returns false.
+
+// render() =>  It is invoked to examine this.props and this.state and return one of the following types: React elements, Arrays and fragments, Booleans or null, String and Number. If shouldComponentUpdate() returns false, the code inside render() will be invoked again to ensure that the component displays itself properly.
+
+// componentDidUpdate() =>  It is invoked immediately after the component updating occurs. In this method, you can put any code inside this which you want to execute once the updating occurs. This method is not invoked for the initial render.
 
 
 
+// (4) =>  Un-Mounting Phase =>  It is the final phase of the react component lifecycle. It is called when a component instance is destroyed and unmounted from the DOM. This phase contains only one method and is given below.
 
+// componentWillUnmount() => This method is invoked immediately before a component is destroyed and unmounted permanently. It performs any necessary cleanup related task such as invalidating timers, event listener, canceling network requests, or cleaning up DOM elements. If a component instance is unmounted, you cannot mount it again.
 
 
 
