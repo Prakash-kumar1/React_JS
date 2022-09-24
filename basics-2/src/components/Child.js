1a23c43ba0869a925e45624f19509d983b521250
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-24 14:23:11 +0530

    De

--- basics-2/src/components/Child.js
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export const Child = () => <p>Child Exporting in WelcomeClass.js . (For WelcomeClass this is a Child , For App WelcomeClass is a Child . For Index, app is a Child)</p>
