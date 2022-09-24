6d5a3fbd11c2c87f5755a1fd6ac8698134c98c70
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-24 15:39:33 +0530

    Changes in the title by using  Hooks { useEffect }  method

--- react-hoo/src/components/TitleChangeHooks.jsx
+++ react-hoo/src/components/TitleChangeHooks.jsx
@@ -1,7 +1,18 @@
-import React , { useState } from "react";
+// @@@####^^&&&****   23-September Sessions   // @@@####^^&&&****
+
+
+// @@@####   Changes in the title by using  { useEffect }  method  ######
+
+import React , { useState , useEffect } from "react";
 
 export function TitleChangeHooks() {
     const [count , setCount] = useState(0) ;
+
+    useEffect( () => {
+// for the first time load (rendering process)
+//  for each  re-Rendering process (whenever my state changes) my useEffect willl get  called
+        document.title = ` ${count} times clicked ` ;
+    }) ;
 
     return(
 <button onClick={ () => setCount(count + 1)}>click- {count} hooks</button>
