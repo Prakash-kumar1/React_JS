d1ae411fdede2328c591f01b41190a1182909221
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-25 18:26:01 +0530

    Error Boundary (21-September Sessions )

--- 9-September/src/components/Error.jsx
+++ 9-September/src/components/Error.jsx
@@ -5,6 +5,20 @@
 //                  that crashed . Error boundaries catch errors during rendering , in lifecycle methods , and 
 //                  in constructors of the whole tree below them .
 
+// A class component becomes an error boundary if it defines either (or both) of the  lifeCycle  methods 
+// static getDerivedStateFromError()  or   componentDidCatch()  .
+
+// Error Boundaries do not catch errors for :
+// (1) =>>  Event Handlers .
+// (2) =>>  Asynchronous code (like setTimeout , Callbacks etc)  .
+// (3) =>>  Server-Side  rendering  .
+// (4) =>>  Error throws in the error boundary itself ( rather than its Children )  .
+
+
+
+// ##########  Watch  Session of  21-September Sessions  by Vasanth sir ############
+
+
 
 
 
