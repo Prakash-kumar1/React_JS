2806dd53ea40b827e362ac3f8ea9ad4570686dfb
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-25 18:08:32 +0530

    Update Error.jsx

--- 9-September/src/components/Error.jsx
+++ 9-September/src/components/Error.jsx
@@ -1,6 +1,9 @@
 // ######### ===>   Error Boundaries  <===##########
 
-
+// Definition ==>>  Error boundaries are React components that catch JavaScript errors anywhere in their child 
+//                  component tree . log those errors and display a fallback UI instead of the componenet tree
+//                  that crashed . Error boundaries catch errors during rendering , in lifecycle methods , and 
+//                  in constructors of the whole tree below them .
 
 
 
