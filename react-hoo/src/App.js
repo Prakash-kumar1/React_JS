50023e3fa406702efc80d6e569a38cf4f3dfcb5d
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-25 14:19:51 +0530

    Importing  Every File Here

--- react-hoo/src/App.js
+++ react-hoo/src/App.js
@@ -1,11 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
-import { Define } from './components/Define';
-import { CounterHooks } from './components/CounterHooks';
-import { StateWithObj } from './components/StateWithObj';
-import { StateWithArr } from './components/StateWithArr';
-import { TitleChangeClass } from './components/TitleChangeClass';
-import { TitleChangeHooks } from './components/TitleChangeHooks';
+// import { Define } from './components/Define';
+// import { CounterHooks } from './components/CounterHooks';
+// import { StateWithObj } from './components/StateWithObj';
+// import { StateWithArr } from './components/StateWithArr';
+// import { TitleChangeClass } from './components/TitleChangeClass';
+// import { TitleChangeHooks } from './components/TitleChangeHooks';
 import { ToDoApp } from './components/ToDoApp';
 
 
@@ -14,12 +14,12 @@ export function App(){
   return (
 
  <>
-  <Define />
+  {/* <Define />
   <CounterHooks />
   <StateWithObj />
   <StateWithArr />
   <TitleChangeClass />
-  <TitleChangeHooks />
+  <TitleChangeHooks /> */}
   <ToDoApp />
   </>
 
