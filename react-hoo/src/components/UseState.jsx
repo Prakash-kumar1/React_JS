9d819cbcb1721c68a431307f380e9f980c411684
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-25 21:36:11 +0530

    useState()  Concepts and examples

---
+++ react-hoo/src/components/UseState.jsx
@@ -0,0 +1,41 @@
+// ######### ===>   useState()  <===##########
+
+// Definition =>>  useState is a hook that allows you to add React state to function components. we call it inside
+//                 a function component to add some local state to it . 
+// useState returns a pair - the current state value and a function that let's you update it. React will preserve 
+// this state between re-Renders . you can call this function from an  event-Handler  or  somewhere else .
+
+// Example ==>>  import React , { useState } from react ;
+ //              const nameStateVariable = useState("Rahul") ;              //Declaring stae variable
+ //              const [name , setName] = useState("Rahul") ; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React , { useState } from react ;
