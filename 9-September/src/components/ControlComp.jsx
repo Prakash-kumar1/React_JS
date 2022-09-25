8b83af165a8bc1513c03b9fffec2f84bc9b8e7eb
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-25 16:26:21 +0530

    Creating Form using Controlled Component

--- 9-September/src/components/ControlComp.jsx
+++ 9-September/src/components/ControlComp.jsx
@@ -4,9 +4,15 @@ export default class ControlComp extends Component {
     constructor(props){
         super(props) ;
         this.state = {
-            value : " "
+            value : " " ,
+            value1 : " Hello there , this is some text in text area " ,
+            name : "Username" ,
+            pwd : " " ,
+            city : "state" ,
+            email : " " ,
         };
     }
+
 
     handleChange =(e) => {
         console.log(e.target.value) ;
@@ -18,15 +24,71 @@ export default class ControlComp extends Component {
     } ;
 
 
+    handleText = (e) => {
+      this.setState({value1 : e.target.value1}) ;
+
+// // if we want to fixed number of characters  or Comments in capital letter
+// this.setState({value1 : e.target.value1.substr(0 , 5)}) ;   //Now , only 100 character prints
+   }
+
+   handleName = (e) => {
+    this.setState({name : e.target.value}) ;
+   }
+
+   handlePassword = (e) => {
+    this.setState({pwd : e.target.value}) ;
+   }
+
+   handleCity = (e) => {
+    this.setState({city : e.target.value}) ;
+   }
+
+   handleEmail = (e) => {
+    this.setState({email: e.target.value}) ;
+   }
+
+
+   handleSubmit = (e) => {
+    console.log(e.target) ;
+    e.preventDefault() ;
+   }
+
+
+
   render() {
     return (
       <div>
-        <form>
+ <input type="text" value={this.state.value} onChange={this.handleChange} /> <br />
+
+{/* onSubmit  always use above with form */}
+
+        <form onSubmit={this.handleSubmit}>         
+
             <h2>Contolled by React</h2>
-            <input type="text" value={this.state.value} onChange={this.handleChange}/>
-            <textarea name="" id="" cols="30" rows="10" value={this.state.value}>Comments</textarea>
+
+            <label>Name :</label> <br />
+            <input type="text" value={this.state.name} onChange={this.handleName}/> <br /> <br />
+
+            <label>Password :</label> <br />
+            <input type="text"  value={this.state.pwd} onChange={this.handlePassword}/> <br /><br />
+
+            <label> Choose your City:</label><br />
+            <input type="text"  value={this.state.city} onChange={this.handleCity}/> <br /><br />
+
+            <label> E-mail ID:</label><br />
+            <input type="text"  value={this.state.email} onChange={this.handleEmail}/> <br /> <br />
+           
+            <textarea cols="30" rows="10" value={this.state.value1} onChange={this.handleText} /> <br /> <br />
+
+            <button type="submit" >Log In</button>
         </form>
+
       </div>
+
     )
   }
 }
+
+
+
+
