4e088b416d6a82ca06fe767da2e28a9fb00a26bf
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-28 11:51:11 +0530

    Creating  Counter  using  setTimeInterval

--- react-hoo/src/components/Check.jsx
+++ react-hoo/src/components/Check.jsx
@@ -10,22 +10,24 @@ import { useState } from "react";
 function Check() {
     const [count , setCount] = useState(0) ;
 
-    const change = async () => {
+    const change = () => {
         console.log("In Change") ;
-        setCount(count + 1) ;
+        setCount((prev) => prev + 1) ;
     } ;
 
     useEffect(() => {
         console.log("In Effect") ;
 let data = setInterval(change , 1000) ;
+// Whenever  you use setInterval , setTimeout , DOM Changes and  web-API's , always clear in return statement . 
 
 
 // This Returning Part is known as " Component will unMount"  OR  " Clean Up"
-    return () => {
-        console.log("Cleaning the Component...") ;
-        clearInterval(data) ;
+
+return () => {
+    console.log("Cleaning the Component...") ;   //In console , we see this many times means Ist component remove then update.
+    clearInterval(data) ;   //In app.js the condition of 10 to 15 it again starts from 0 due to clearInterval .
     } ;
-    } , [count]) ;
+    } , []) ;
 
 
 return <div>{count}</div> ;
