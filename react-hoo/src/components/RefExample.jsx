5389ce0506d13c0919751d9465e3b1b4dadf7c05
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-28 12:47:47 +0530

    Update RefExample.jsx

--- react-hoo/src/components/RefExample.jsx
+++ react-hoo/src/components/RefExample.jsx
@@ -1,20 +1,37 @@
-import React , {useState , useEffect} from "react";
+// ###########    27 - September sessions =>>  useRef (it returns a Object)########### 
+
+
+import React , {useState , useEffect ,useRef} from "react";
+
 
 export function RefExample() {
     const[name , setName]  =  useState("") ;
-    const[count , setCount]  =  useState(0) ;
+    // const[count , setCount]  =  useState(0) ;     //Or we can use
+    const countObj = useRef(1) ;   //whatever i am passing as argument it becomes current data , see in console
 
 
+    // useEffect(() => {
+    //     setCount((prev) => prev + 1) ;        
+    // } , [name])   //whenever name changing , i can do count+1
+
+
+// In useRef , we don't change any state , it means whenever useRef changes my component will not be  Re-render .
+// so, there is no need to use [name] ..
     useEffect(() => {
-        setCount((prev) => prev + 1) ;
-    } , [name])   //whenever name changing , i can do count+1
+        // console.log(countObj) ;
+        countObj.current = countObj.current + 1 ;
+        console.log(countObj.current) ;
+    })
+
+
 
 
     return (
     <div>
         <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
         <p>this is {name}</p>
-        <p>rendered {count} times</p>
+        {/* <p>rendered use-state method {count} times</p>    */}
+        <p>rendered use - ref method {countObj.current} times</p>   
     </div>
     )
 }
