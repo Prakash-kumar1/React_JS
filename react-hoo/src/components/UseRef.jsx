774f2a78351c0dd897b3aa6860cc122166f40598
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-28 21:49:03 +0530

    Update UseRef.jsx

--- react-hoo/src/components/UseRef.jsx
+++ react-hoo/src/components/UseRef.jsx
@@ -1,15 +1,27 @@
 // Definition of useRef ==>>  when to need to manipulate DOM tree . sometimes we can't work with state and props
 
 
-import React , {useState , useEffect ,useRef} from "react";
+import React , {useRef} from "react";
 
 export function UseRef() {
+
     let inputRef = useRef(null)
+
 
     function handleInput() {
         console.log("function called") ;
-        inputRef.current.value = "1000" ;
+// Applying  style  properties
+
+        // inputRef.current.value = "1000" ;
+        // inputRef.current.focus()  ;
+        // inputRef.current.style.color = "red" ;
+        inputRef.current.style.display = "none" ;
+
+
     }
+
+
+
     return (
         <div>
             <h1> use-Ref in React-JS  </h1>
