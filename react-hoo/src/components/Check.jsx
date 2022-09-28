e97ae32414238b1808f8cd4985e0503ba1023e9a
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-28 10:35:43 +0530

    starting 27 september  sessions

---
+++ react-hoo/src/components/Check.jsx
@@ -0,0 +1,27 @@
+import React from "react" ;
+import { useEffect } from "react";
+import { useState } from "react";
+
+function Check() {
+    const [count , setCount] = useState(0) ;
+
+    const change = async () => {
+        console.log("In Change") ;
+        setCount(count + 1) ;
+    } ;
+
+    useEffect(() => {
+        console.log("In Effect") ;
+let data = setInterval(change , 1000) ;
+
+    return () => {
+        clearInterval(data) ;
+    } ;
+    } , [count]) ;
+
+
+return <div>{count}</div> ;
+
+}
+
+export default Check ;
