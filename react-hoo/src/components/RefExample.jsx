db7df2c776b80e20bd37d85b62c146d36ecca71b
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-28 12:11:56 +0530

    Update RefExample.jsx

--- react-hoo/src/components/RefExample.jsx
+++ react-hoo/src/components/RefExample.jsx
@@ -1,5 +1,30 @@
-import React , {useState} from "react";
+import React , {useState , useEffect} from "react";
 
 export function RefExample() {
-    return <div>Ref-example</div>
+    const[name , setName]  =  useState("") ;
+    const[count , setCount]  =  useState(0) ;
+
+
+    useEffect(() => {
+        setCount((prev) => prev + 1) ;
+    } , [name])   //whenever name changing , i can do count+1
+
+
+    return (
+    <div>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+        <p>this is {name}</p>
+        <p>rendered {count} times</p>
+    </div>
+    )
 }
+
+
+
+
+
+
+
+
+
+
