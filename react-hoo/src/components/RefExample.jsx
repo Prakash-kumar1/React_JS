9c11782accd2827095101935972a7757456e29fc
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-28 13:04:09 +0530

    Update RefExample.jsx

--- react-hoo/src/components/RefExample.jsx
+++ react-hoo/src/components/RefExample.jsx
@@ -16,6 +16,7 @@ export function RefExample() {
     // } , [name])   //whenever name changing , i can do count+1
 
 
+
 // In useRef , we don't change any state , it means whenever useRef changes my component will not be  Re-render .
 // so, there is no need to use [name] ..
     useEffect(() => {
@@ -25,16 +26,19 @@ export function RefExample() {
         console.log(inputRef.current) ;
 // Changing Default value
 inputRef.current.value = "Prakash"  ;
+inputRef.current.style.border = "14px double red"  ;
 inputRef.current.focus() ;
     })
 
-
+// having UI operation it is good to use   =>> " useRef "
 
 
     return (
     <div>
-        <input type="text" value={name} ref={inputRef} onChange={(e) => setName(e.target.value)}/>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+        <input ref={inputRef}  type="text" value={name} onChange={(e) => setName(e.target.value)}/>
         <p>this is {name}</p>
+        <button onClick={() => (inputRef.current.value = "changing data using useRef")}>Click</button>
         {/* <p>rendered use-state method {count} times</p>    */}
         <p>rendered use - ref method {countObj.current} times</p>   
     </div>
