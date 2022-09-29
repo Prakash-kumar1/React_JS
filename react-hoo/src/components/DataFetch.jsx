52c375bab8acb9ffff78ed28291db0ab3c4dcf1f
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-29 12:50:28 +0530

    Update DataFetch.jsx

--- react-hoo/src/components/DataFetch.jsx
+++ react-hoo/src/components/DataFetch.jsx
@@ -1,15 +1,35 @@
 // ######   Starting  28 - september sessions   ######
 
 import React , { useState , useEffect }  from  "react" ;
-import  axios   from  " axios "  ;
+import  axios   from  "axios"  ;
 
 export function DataFetch() {
-    const [posts , setPosts]  =  useState( [] ) ;
+    const [posts , setPosts]  =  useState([]) ;
 
     useEffect(() => {
-    console.log  (axios.get("https://jsonplaceholder.typicode.com/posts"))
+
+    axios
+    .get("https://jsonplaceholder.typicode.com/posts")
+    // .then((response) => console.log(response)) 
+    .then((response) => setPosts([...response.data])) ;
+
     }) ;
 
-    return <div>DataFEtching</div>
+    return (
+        <div>
+
+       <h1> Data-Fetching</h1>
+       {posts.map((post) =>(
+        <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+            
+        </div>
+       ))}
+
+        </div> 
+
+    )
+    
 }
 
