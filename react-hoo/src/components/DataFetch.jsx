4b0ca70c0c39f0776852e0c33a2f0359d8fabc2c
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-29 13:13:11 +0530

    Update DataFetch.jsx

--- react-hoo/src/components/DataFetch.jsx
+++ react-hoo/src/components/DataFetch.jsx
@@ -4,32 +4,60 @@ import React , { useState , useEffect }  from  "react" ;
 import  axios   from  "axios"  ;
 
 export function DataFetch() {
-    const [posts , setPosts]  =  useState([]) ;
+    // const [posts , setPosts]  =  useState([]) ;
+
+// $$$ --->>>>  ====>>>  only for one data 
+const [post , setPost]  =  useState([]) ;
+const [id , setId]  =  useState(1) ;
+
 
     useEffect(() => {
-
     axios
-    .get("https://jsonplaceholder.typicode.com/posts")
-    // .then((response) => console.log(response)) 
-    .then((response) => setPosts([...response.data])) ;
-
+    .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    .then((response) => setPost(response.data)) 
+    .catch((err) => console.log(err)) ;
     }) ;
+
+
+// $$$ --->>>>  ====>>>  for All the data 
+
+    // useEffect(() => {
+
+    // axios
+    // .get("https://jsonplaceholder.typicode.com/posts")
+    // // .then((response) => console.log(response)) 
+    // .then((response) => {
+    //     console.log(response.data) ;
+    //     setPost([...response.data]) ;
+
+    // }) ;
+
+    // } , []) ;
+
+
 
     return (
         <div>
 
-       <h1> Data-Fetching</h1>
+{/* // $$$ --->>>>  ====>>>  only for one data  */}
+       <h1> Data-Fetching for a single ID</h1>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+
+
+{/* // $$$ --->>>>  ====>>>  for all the data  */}
+      {/* <h1> Data-Fetching</h1>
        {posts.map((post) =>(
         <div key={post.id}>
-            <h1>{post.title}</h1>
+            <h2>{post.title}</h2>
             <p>{post.body}</p>
-            
+
         </div>
-       ))}
+       ))} */}
 
         </div> 
 
-    )
+    ) ;
     
 }
 
