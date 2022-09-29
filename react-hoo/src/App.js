2b41726dfe2c8f7d96ee474d27d628d078974ad9
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-29 15:08:01 +0530

    Importing all files  and  use-Context method

--- react-hoo/src/App.js
+++ react-hoo/src/App.js
@@ -1,10 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
+import React ,{ useState } from 'react';
+
 // import { DataFetch } from './components/DataFetch';
 // import { UseRef } from './components/UseRef';
 // import { RefExample } from './components/RefExample';
 // import Check from './components/Check';
-// import { useState } from 'react';
 // import UseCustomHook from './components/UseCustomHook';
 // import { Define } from './components/Define';
 // import { CounterHooks } from './components/CounterHooks';
@@ -12,9 +13,15 @@ import './App.css';
 // import { StateWithArr } from './components/StateWithArr';
 // import { TitleChangeClass } from './components/TitleChangeClass';
 // import { TitleChangeHooks } from './components/TitleChangeHooks';
-// import { ToDoApp } from './components/ToDoApp';
 // import { UseState } from './components/UseState';
 // import { UseEffect } from './components/UseEffect';
+
+import { ToDoApp } from './components/ToDoApp';
+
+// import { ComponentA } from './components/ComponentA';
+// export const AgeContext = React.createContext() ;
+// export const PlaceContext = React.createContext() ;
+// export const FoodContext = React.createContext() ;
 
 
 export function App(){
@@ -23,6 +30,13 @@ export function App(){
 
 // // For Check.jsx which keep increaseing on a certain  Time - Interval 
 //          const [valid , setValid]  =  useState(0) ;
+
+// // Passing Props in component C 
+// const [name , setName]  =  useState("Prakash ") ;
+// const [age , setAge]  =  useState("23 ") ;
+// const [place , setPlace]  =  useState("Uttar Pradesh ") ;
+// const [food , setFood]  =  useState("Dosa , Chicken Biryani ") ;
+
 
   return (
 
@@ -36,21 +50,33 @@ export function App(){
   <UseEffect />
   <UseState />
   <UseRef />
-<ToDoApp />
+  < DataFetch />
+  < RefExample /> 
 <Check />    */}
+
+<ToDoApp />
 
   {/* Using  Check.jsx  with  UseState  in App.js */}
   {/* {(valid < 10 || valid > 15) && <Check />}
   <button onClick={() => setValid((prev) => prev+1)}>click = {valid}</button> */}
 
+
 {/* // For our custom hooks */}
   {/* <h3>count Up : {data.count}</h3>
-  <button type="button" onClick={data.handleIncrement}>addBy5</button>
+  <button type="button" onClick={data.handleIncrement}>addBy5</button>*/}
 
-  < DataFetch />
+{/* ####-=-=-=->>>>  Use- Context Method   ####-=-=-=->>>>*/}
+{/* < ComponentA name={name}/> */}
+{/* if i want to pass in Component-C , first pass in Component-A , then in Component-B and at last In Component-C */}
+{/* This is a Lengthy Process also not good practice  */}
 
-  < RefExample /> */}
-
+{/* <AgeContext.Provider value={age}>
+<PlaceContext.Provider value={place}>
+<FoodContext.Provider value={food}>
+< ComponentA />
+</FoodContext.Provider>
+</PlaceContext.Provider>
+</AgeContext.Provider> */}
 
   </>
 
