eb0969838b15bb7cc124ce4339d55fa2ebed4f0d
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-29 12:35:12 +0530

    Update DataFetch.jsx

--- react-hoo/src/components/DataFetch.jsx
+++ react-hoo/src/components/DataFetch.jsx
@@ -1,4 +1,15 @@
 // ######   Starting  28 - september sessions   ######
 
+import React , { useState , useEffect }  from  "react" ;
+import  axios   from  " axios "  ;
 
+export function DataFetch() {
+    const [posts , setPosts]  =  useState( [] ) ;
+
+    useEffect(() => {
+    console.log  (axios.get("https://jsonplaceholder.typicode.com/posts"))
+    }) ;
+
+    return <div>DataFEtching</div>
+}
 
