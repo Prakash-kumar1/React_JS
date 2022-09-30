4666ddada9f14c9b6f69f6bc694955f8575452f5
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-09-30 18:20:03 +0530

    an example of Class Based Component

---
+++ 6-September/src/Profile.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Profile extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h1 className="class">Profile.js is an example of Class Based Component . </h1>
+            </div>
+        )
+    }
+}
+
+export default Profile ;
