998213f6163291dcf6b3e34f84d1cf82af312430
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-02 15:06:37 +0530

    ########==>>   02 October Sunday  React - Test   ########==>>

--- 6-September/src/App.js
+++ 6-September/src/App.js
@@ -1,21 +1,24 @@
 // import logo from './logo.svg';
 
 import './App.css';
-import {Toggle} from './components/Toggle.js'
+// import {Toggle} from './components/Toggle.js'
 // inside curly braces toggle represents class
 
-import Button from './components/Button';
-import Prop from './Prop';
-import State from './State';
-import Profile from "./Profile";
-import Func from "./Func";
+// import Button from './components/Button';
+// import Prop from './Prop';
+// import State from './State';
+// import Profile from "./Profile";
+// import Func from "./Func";
+import { Corona } from './components/Corona';
 
 
 function App() {
   return (
     <section>
 
-      <Toggle name="Present in props , console" />
+      < Corona />
+
+      {/* <Toggle name="Present in props , console" />
 
       <Button text="Delete" class="delete" />
       <Button text="Save" class="save" />
@@ -24,18 +27,17 @@ function App() {
       <Func text = "Functional Component example"/>
 
 
-<Prop 
+<Prop  */}
 
-// Passing Props in Functional component
-text="Passing data in props" 
-obj={{name:'Rinki' , food:'Dosa' , note: 'Passing Object in two curly Braces'}}
+ {/* Passing Props in Functional component */}
+ {/* text="Passing data in props"  */}
+{/* obj={{name:'Rinki' , food:'Dosa' , note: 'Passing Object in two curly Braces'}} */}
 
-// Passing Props in Class component
-text1="Passing data in class component always used with this keyword"
-obj1={{name:'Prakash' , food:'Chicken' , note: 'Passing Object in two curly Braces'}}
-/>
+ {/* Passing Props in Class component */}
+ {/* text1="Passing data in class component always used with this keyword" */}
+{/* obj1={{name:'Prakash' , food:'Chicken' , note: 'Passing Object in two curly Braces'}}/> */}
 
-<State />
+{/* <State /> */}
 
     </section>
   );
---
+++ 6-September/src/components/Corona.jsx
@@ -0,0 +1,71 @@
+// ########==>>   02 October Sunday  React - Test   ########==>>
+
+import React, { useState, useEffect } from 'react';
+import './Covid.css';
+
+
+export function Corona() {
+    const [data, setData] = useState([]);
+    const [theme, setTheme] = useState('dark-mode');
+
+    useEffect(() => {
+        document.body.className = theme;
+
+    }, [theme]);
+
+    const toggle = () => {
+        if (theme === "dark-mode") {
+            setTheme('light-mode');
+        } else {
+            setTheme('dark-mode');
+        }
+    }
+
+    const getCoviddata = async () => {
+        try {
+            const res = await fetch('https://api.covid19api.com/summary');
+            const actualData = await res.json();
+            console.log(actualData.Global);
+            setData(actualData.Global)
+        } catch (err) {
+            console.log(err);
+        }
+
+    }
+
+    useEffect(() => {
+        getCoviddata();
+    }, []);
+
+    return (
+        <>
+            <div class="container">
+                <h1 class="main-heading">Covid 19 Live Data Update</h1>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th class="tr">New Confirmed</th>
+                            <th class="tr">New Deaths</th>
+                            <th class="tr">New Recovered</th>
+                            <th class="tr">Total Cases</th>
+                            <th class="tr">Total Deaths</th>
+                            <th class="tr">Total Recovered</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <td class="data">{data['NewConfirmed']}</td>
+                        <td class="data">{data['NewDeaths']}</td>
+                        <td class="data">{data['NewRecovered']}</td>
+                        <td class="data">{data['TotalConfirmed']}</td>
+                        <td class="data">{data['TotalDeaths']}</td>
+                        <td class="data">{data['TotalRecovered']}</td>
+                    </tbody>
+                </table>
+                <div className='toggleDiv'>
+                    <p>Date & Time : {data.Date}</p>
+                    <button class="btn" onClick={() => toggle()}>Toggle Mode</button>
+                </div>
+            </div>
+        </>
+    )
+}
