7a682ee75ed37222153d94bbdcea441db050c42f
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-04 12:24:02 +0530

    Update ReducerCounter.jsx

---
+++ react-hoo/src/components/ReducerCounter.jsx
@@ -0,0 +1,12 @@
+import React, {useReducer} from "react";
+// useReducer is also used to store and handle state data
+// useState  is built  on  useReducer .
+
+
+export function ReducerCounter(){
+
+}
+
+
+
+
