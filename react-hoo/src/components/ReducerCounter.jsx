18a04ad8ef071e82fc0c0fc8649893552996c027
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-04 12:58:14 +0530

    Update ReducerCounter.jsx

--- react-hoo/src/components/ReducerCounter.jsx
+++ react-hoo/src/components/ReducerCounter.jsx
@@ -1,16 +1,59 @@
 import React, {useReducer} from "react";
-// useReducer is also used to store and handle state data
-// useState  is built  on  useReducer .
+// // useReducer is also used to store and handle state data
+// // useState  is built  on  useReducer .
  
 
-const intialState = 0 ;
+// const intialState = 0 ;
+// const reducer = (state , action) => {
+//     switch(action) {
+//         case 'increment' :
+//             return state + 3 ;
+
+//         case 'decrement' :
+//             return state - 1 ;
+
+//         case 'reset' :
+//             return intialState ;
+
+//         default :
+//             return state ;
+//     }
+// } ;
+
+
+// export function ReducerCounter(){
+//     const [count , dispatch] = useReducer(reducer , intialState)
+
+//     return ( 
+//         <>
+
+//         <h1>Count = {count}</h1>
+//         <button onClick={() => dispatch("increment")}>Add</button>
+//         <button onClick={() => dispatch("decrement")}>Subtract</button>
+//         <button onClick={() => dispatch("reset")}>reset</button>
+        
+//         </>
+//     )
+// }
+
+
+
+
+// ##########===>>  In Case of  Objects  <<==##########
+
+
+
+const intialState = {
+    value : 0 ,
+};
+
 const reducer = (state , action) => {
     switch(action) {
         case 'increment' :
-            return state + 1 ;
+            return {...state , value : state.value + 1} ;
 
         case 'decrement' :
-            return state - 1 ;
+            return state - 2 ;
 
         case 'reset' :
             return intialState ;
@@ -22,12 +65,12 @@ const reducer = (state , action) => {
 
 
 export function ReducerCounter(){
-    const [count , dispatch] = useReducer(reducer , intialState)
+    const [countObj , dispatch] = useReducer(reducer , intialState.value)
 
     return ( 
         <>
 
-        <h1>Count = {count}</h1>
+        <h1>Count = {countObj}</h1>
         <button onClick={() => dispatch("increment")}>Add</button>
         <button onClick={() => dispatch("decrement")}>Subtract</button>
         <button onClick={() => dispatch("reset")}>reset</button>
