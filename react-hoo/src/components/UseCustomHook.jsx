915832efcf53bdfc87bc29d6032c739af37967d5
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-05 19:36:29 +0530

    custom hook  Definitions and  Examples

--- react-hoo/src/components/UseCustomHook.jsx
+++ react-hoo/src/components/UseCustomHook.jsx
@@ -3,7 +3,7 @@
 // Definition =>>  A custom hook is a Javascript function , when we want to share logic between  two  JS  Functions
 //                 ,we extract it to a 3rd  function .
 //          Building your own hooks let's you extract component logic into reuseable functions .
-// You can write custom Hooks that cover a wide range of use cases like form handling , animation , timers ,
+// You can write custom Hooks that cover a wide range of use cases like =>>  form handling , animation , timers ,
 // Declarative Subscriptions  and  many more .
 
 
