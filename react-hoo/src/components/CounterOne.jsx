299916b2145b8152b32383773910f787d8515258
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-05 20:31:54 +0530

    d

--- react-hoo/src/components/CounterOne.jsx
+++ react-hoo/src/components/CounterOne.jsx
@@ -2,21 +2,25 @@
 
 
 import React , {useState} from "react";
+import { useCounter } from "./hooks/useCounter";
 
 export function CounterOne() {
-    const [count , setCount] = useState(0) ;
+    // const [count , setCount] = useState(0) ;
 
-    const increment = () => {
-        setCount((prev) => prev + 1) ;
-    } ;
+    // const increment = () => {
+    //     setCount((prev) => prev + 1) ;
+    // } ;
 
-    const decrement = () => {
-        setCount((prev) => prev - 1) ;
-    } ;
-    const reset = () => {
-        setCount(0) ;
-    } ;
+    // const decrement = () => {
+    //     setCount((prev) => prev - 1) ;
+    // } ;
+    // const reset = () => {
+    //     setCount(0) ;
+    // } ;
 
+// When you are importing from  UseCounter.jsx
+    const [count , increment , decrement , reset] = useCounter() ;
+     
 return(
     <>
     <h2>count - {count}</h2>
