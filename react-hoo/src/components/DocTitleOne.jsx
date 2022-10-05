65ff7673089b5a56722479cd564f11674576381b
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-05 20:00:25 +0530

    Create DocTitleOne.jsx

---
+++ react-hoo/src/components/DocTitleOne.jsx
@@ -0,0 +1,25 @@
+// ######### =>  04-October Session   ######### =>
+
+import React, {useState} from "react";
+import { useUpdateTitle } from "./hooks/useUpdateTitle";
+
+
+export function DocTitleOne() {
+    const [count , setCount] = useState(0) ;
+    useUpdateTitle(count) ;
+
+    return (
+        <div>
+            <button onClick={() => setCount((prev) => prev + 1)}>click - {count}</button>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
