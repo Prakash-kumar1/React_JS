ae69f60ee5773c35f0cb8a741ab8152de838a2c9
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-07 17:22:02 +0530

    Update MemoCounter.jsx

--- react-hoo/src/components/MemoCounter.jsx
+++ react-hoo/src/components/MemoCounter.jsx
@@ -8,9 +8,18 @@ import React, {useState , useMemo} from 'react'
 
 export  function MemoCounter() {
 
-// ##@@ Example - 1   Craeting  Two  Counter  
+// ##@@ Example - 1   Craeting  Two  Counter Functions 
     const [counterOne , setCounterOne] = useState(0)
     const [counterTwo , setCounterTwo] = useState(0)
+
+// ##@@ Example - 2   Craeting  one Factorial and  Input  Functions 
+const [counter , setCounter] = useState(1) ;
+const result = useMemo(() => {
+    return factorial(counter) ;
+})
+const [name , setName] = useState(" ") ;
+
+
 
 const incrementOne = () => {
     setCounterOne((prevCounterOne) => prevCounterOne+1) ;
@@ -28,6 +37,22 @@ const isEven = useMemo(() => {
     return counterOne % 2 === 0 ;
 }, [counterOne]);
 
+
+
+function factorial(n) {
+    let i = 0 ;
+    while(i < 1000000000) i++ ;
+    if(n<0) {
+        return -1 ;
+    }
+    if(n == 0) {
+        return 1 ;
+    }
+
+    return  n *  factorial(n - 1) ;
+}
+
+
   return (
     <>
     <div>
@@ -38,6 +63,24 @@ const isEven = useMemo(() => {
     <div>
     <button onClick={incrementTwo} >counterTwo = {counterTwo}</button> 
     </div>
+
+
+    {/* // ##@@ Example - 2   Craeting  one Factorial and  Input  Functions  */}
+
+    <div>
+        <h1>Factorial  of {counter}  is: <span>{result}</span> </h1>
+    </div>
+    <div>
+    <button onClick={() => setCounter(counter - 1)} >Decrement</button> 
+    <button onClick={() => setCounter(counter + 1)} >Increment</button> 
+    </div>
+    <div>
+        <label>Enter Name</label>
+        <input type="text" value={name} placeholder="Enter Name"  onChange={(e) => setName(e.target.value)}/>
+        <p>{ `My name is ${name}` }</p>
+    </div>
+
     </>
   ) ;
 }
+
