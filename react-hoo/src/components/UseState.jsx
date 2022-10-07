73d10f8ff6e9260941d2e85c58daa3fe1b42e975
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-07 16:24:15 +0530

    useState ( )   Definition  and  Examples

--- react-hoo/src/components/UseState.jsx
+++ react-hoo/src/components/UseState.jsx
@@ -66,7 +66,7 @@ export function UseState() {
     const addNames = (e) => {
         e.preventDefault() ;
         setNames([ ...names , { id : names.length , name} ])
-        setName(" ") ;
+        setName(" ") ;       //It will make input box empty again for new values
     }
 
 
