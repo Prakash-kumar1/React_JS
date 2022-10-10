260216b19c5a1a4c1324407f3287f51642f61b13
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-10 22:24:32 +0530

    Update App.js

--- react-hoo/src/App.js
+++ react-hoo/src/App.js
@@ -79,7 +79,7 @@ const [food , setFood]  =  useState("Dosa , Chicken Biryani ") ;
 
 {/* ####-=-=-=->>>>  Use- Context Method   ####-=-=-=->>>>*/}
 
-{/* < ComponentA name={name}/> */}
+< ComponentA name={name}/>
 
 {/* if i want to pass in Component-C , first pass in Component-A , then in Component-B and at last In Component-C
 This is a Lengthy Process also not good practice  */}
