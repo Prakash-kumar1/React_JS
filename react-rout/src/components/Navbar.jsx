d123c540c2494b81d1747ae5c8c2d789baa2190e
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-13 20:58:41 +0530

    routes

--- react-rout/src/components/Navbar.jsx
+++ react-rout/src/components/Navbar.jsx
@@ -4,18 +4,19 @@ const Navbar = () => {
     return(
         <>
         <ul>
-            <li> <a href="/">Home</a> </li>
-            <li> <a href="/about"> about</a></li>
-            <li> <a href="/contact">contact</a> </li>
-            <li><a href="/Blog">Blog</a></li>
-            <li><a href="/Connect">Connect</a></li>
-            <li><a href="/Service">Service</a></li>
+            <li> <a href="/">Home </a> </li>
+            <li> <a href="/about"> about </a> </li>
+            <li> <a href="/contact">contact </a> </li>
+            <li> <a href="/Blog">Blog </a></li> 
+            <li> <a href="/Connect">Connect </a> </li>
+            <li> <a href="/Service">Service </a> </li>
         </ul>
+        
         <ul className='list'>
             
-            <li><Link to="/Blog">Blog</Link></li>
-            <li><Link to="/Connect">Connect</Link></li>
-            <li><Link to="/Service">Service</Link></li>
+            <li><Link to="/Blog">Blog </Link></li>
+            <li><Link to="/Connect">Connect </Link></li>
+            <li><Link to="/Service">Service </Link></li>
 
         </ul>
         </>
