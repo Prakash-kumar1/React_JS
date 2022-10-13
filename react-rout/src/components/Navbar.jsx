00acabf4a5547cd211f48757b6a8508b75ad80ea
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-13 22:51:44 +0530

    Update Navbar.jsx

--- react-rout/src/components/Navbar.jsx
+++ react-rout/src/components/Navbar.jsx
@@ -1,26 +1,46 @@
-import {Link} from 'react-router-dom';
+import {Link , NavLink} from 'react-router-dom';
 
 const Navbar = () => {
     return(
         <>
 
 {/* In this case , Page Reloads  or  Refresh */}
-        <ul>
+        {/* <ul>
             <li> <a href="/">Home </a> </li>
             <li> <a href="/about"> about </a> </li>
             <li> <a href="/contact">contact </a> </li>
             <li> <a href="/Blog">Blog </a></li> 
             <li> <a href="/Connect">Connect </a> </li>
             <li> <a href="/Service">Service </a> </li>
-        </ul>
+        </ul> */}
 
 
-{/* For without Reloading or Refreshing , we use  Link tag */}
-        <ul className='list'>
+{/* For special styling , we use  NavLink tag */}
+        <ul className='style'>
             
-            <li><Link to="/Blog">Blog2 </Link></li>
+            <li><Link to="/Blog" >Blog2 </Link></li>
             <li><Link to="/Connect">Connect2 </Link></li>
             <li><Link to="/Service">Service2 </Link></li>
+
+        </ul>
+
+        <ul className='list'>
+            
+            <li><NavLink to="/Blog"  style={({isActive}) => {
+                return {
+                    backgroundColor : isActive ? 'red' : 'magenta'
+                }
+            }}>Blog-3 </NavLink></li>
+            <li><NavLink to="/Connect" style={({isActive}) => {
+                return {
+                    backgroundColor : isActive ? 'green' : 'blue'
+                }
+            }}>Connect-3 </NavLink></li>
+            <li><NavLink to="/Service"  style={({isActive}) => {
+                return {
+                    backgroundColor : isActive ? 'black' : 'gold'
+                }
+            }}>Service-3 </NavLink></li>
 
         </ul>
         </>
