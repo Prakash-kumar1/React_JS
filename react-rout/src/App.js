9e60f8329833c9fdc40ebc3c9015036ef1da8518
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-13 20:09:00 +0530

    Update App.js

--- react-rout/src/App.js
+++ react-rout/src/App.js
@@ -1,14 +1,22 @@
 import { Routes , Route} from 'react-router-dom';
 import './App.css';
+import Blog from './components/pages/Blog';
+import Service from './components/pages/Service';
+import Connect from './components/pages/Connect';
 
 export function App(){
 
-
   return (
     <Routes>
+
       <Route  path = '/'  element  =  {<div> I am Home Page , slash means by default </div>}/>
       <Route  path = '/contact'  element =  {<div> I am Contact Page , you are coming from Home page </div>}/>
       <Route  path = '/about'  element  =  {<div> I am About Page . Do you want to say something about me   </div>}/>
+      
+      <Route  path = '/Blog'  element  =  {<Blog />} />
+      <Route  path = '/Service'  element  =  {<Service />} />
+      <Route  path = '/Connect'  element  =  {<Connect />} />
+
     </Routes>
 
   );
