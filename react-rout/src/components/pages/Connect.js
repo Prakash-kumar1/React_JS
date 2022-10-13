b4c3fa3e5db59e3db7b93d8712fef5183312e671
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-13 20:09:27 +0530

    connection page

---
+++ react-rout/src/components/pages/Connect.js
@@ -0,0 +1,9 @@
+const Connect = () => {
+    return(
+        <>
+        <h1>You can connect with me on LinkedIn , Email , Twitter  and Instagram .</h1>
+        </>
+    )
+} ;
+
+export default Connect ;
