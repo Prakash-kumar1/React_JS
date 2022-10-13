6bf514e14f3fef63cbe81514283830ecfe7c63a8
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-13 19:48:20 +0530

    Update App.js

--- react-rout/src/App.js
+++ react-rout/src/App.js
@@ -6,9 +6,9 @@ export function App(){
 
   return (
     <Routes>
-      <Route path='/'element={<div> I am Home Page , slash means by default </div>}/>
-      <Route path='/contact'element={<div> I am Contact Page , you are coming from Home page </div>}/>
-      <Route path='/about'element={<div> I am About Page . Do you want to say something about me   </div>}/>
+      <Route  path = '/'  element  =  {<div> I am Home Page , slash means by default </div>}/>
+      <Route  path = '/contact'  element =  {<div> I am Contact Page , you are coming from Home page </div>}/>
+      <Route  path = '/about'  element  =  {<div> I am About Page . Do you want to say something about me   </div>}/>
     </Routes>
 
   );
