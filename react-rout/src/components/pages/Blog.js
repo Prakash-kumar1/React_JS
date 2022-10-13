521b8d4e79ac3ad6b2715799cf07ee7a24973163
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-13 20:09:11 +0530

    Blog page

---
+++ react-rout/src/components/pages/Blog.js
@@ -0,0 +1,17 @@
+// import React from 'react'
+
+// export default function Blog() {
+//   return (
+//     <div> This is my Blog page . </div>
+//   )
+// }
+
+const Blog = () => {
+    return(
+        <>
+        <h1>This is my Blog page .</h1>
+        </>
+    )
+} ;
+
+export default Blog ;
