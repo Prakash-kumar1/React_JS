839841cce2b998a336e8f7b61a213fe38d55e456
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-13 11:50:01 +0530

    Basic concepts

--- react-rout/src/App.js
+++ react-rout/src/App.js
@@ -1,13 +1,15 @@
+import { Routes , Route} from 'react-router-dom';
 import './App.css';
 
 export function App(){
 
 
   return (
-
-    <>
-    
-  </>
+    <Routes>
+      <Route path='/'element={<div> I am Home Page , slash means by default </div>}/>
+      <Route path='/contact'element={<div> I am Contact Page , you are coming from Home page </div>}/>
+      <Route path='/about'element={<div> I am About Page . Do you want to say something about me   </div>}/>
+    </Routes>
 
   );
 
