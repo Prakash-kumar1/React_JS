6efca7481f2113a6090d337cdcff5982d6e8fd85
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-14 12:55:30 +0530

    LogOut page

---
+++ react-rout/src/components/pages/Logout.js
@@ -0,0 +1,9 @@
+const Logout = () => {
+    return(
+        <>
+        <h1>You can connect with me on LinkedIn , Email , Twitter  and Instagram .</h1>
+        </>
+    )
+} ;
+
+export default Logout ;
