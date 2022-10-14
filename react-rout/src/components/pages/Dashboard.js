2fca11fa20a7c0a4b8b149e0be0536b5df0131b9
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-14 13:14:18 +0530

    l

--- react-rout/src/components/pages/Dashboard.js
+++ react-rout/src/components/pages/Dashboard.js
@@ -2,11 +2,14 @@ import { useNavigate } from "react-router-dom";
 
 
 const Dashboard = () => {
-    let Navigate = useNavigate()
+    let navigate = useNavigate() ;
+    let data = {
+        'text' : "passing this data from dashboard"
+    }
     return(
         <>
         <h1>When  isLogged = true ; only then you can see this Dashboard page .</h1>
-        <button onClick={()=>{Navigate("/Logout")}} >LogOut</button>
+        <button onClick={()=>{navigate("/Logout" , {state:data}) }} >LogOut</button>
         </>
     )
 } ;
--- react-rout/src/components/pages/Logout.js
+++ react-rout/src/components/pages/Logout.js
@@ -1,7 +1,15 @@
+// For Receiving the data , we use ( useLocation )
+import { useLocation } from "react-router-dom";
+
 const Logout = () => {
+
+    let location1 = useLocation()
+    console.log(location1) ;
+
     return(
         <>
         <h3>This is logout page . we are using useNavigate hook to come on this Page</h3>
+        <h4>{location1.state.text} Logged Out!!</h4>
         </>
     )
 } ;
