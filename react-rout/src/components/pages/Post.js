b5809770ce695156681e751a0c24e372a412c5c2
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-14 12:04:09 +0530

    Update Post.js

--- react-rout/src/components/pages/Post.js
+++ react-rout/src/components/pages/Post.js
@@ -1,7 +1,13 @@
+// For getting user's data
+import { useParams } from "react-router-dom";
+
 const Post = () => {
+    let {category} = useParams() 
     return(
         <>
         <h1>This is an example for error pages.</h1>
+{/* getting user's data */}
+<h3><em>Post Page - {category}</em></h3>
         </>
     )
 } ;
