b1c431103e4a4d4bde913caeed8a74f00a486402
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-15 13:56:40 +0530

    import { Routes , Route, Navigate} from 'react-router-dom';

--- react-rout/src/App.js
+++ react-rout/src/App.js
@@ -1,36 +1,36 @@
 import { Routes , Route, Navigate} from 'react-router-dom';
 import './App.css';
 
-// import Blog from './components/pages/Blog';
-// import Service from './components/pages/Service';
-// import Connect from './components/pages/Connect';
-// import Dashboard from './components/pages/Dashboard';
-// import Login from './components/pages/Login';
-// import Logout from './components/pages/Logout';
-// import Post from './components/pages/Post';
-// import Navbar from './components/Navbar';
+import Blog from './components/pages/Blog';
+import Service from './components/pages/Service';
+import Connect from './components/pages/Connect';
+import Dashboard from './components/pages/Dashboard';
+import Login from './components/pages/Login';
+import Logout from './components/pages/Logout';
+import Post from './components/pages/Post';
+import Navbar from './components/Navbar';
 
 
-import {Post} from './components/project/Post';
-import About from './components/project/About';
-import {Contact} from './components/project/Contact';
-import Home from './components/project/Home';
-import Layout from './components/Layout';
-import PostLayout from './components/PostLayout';
-import PostDetail from './components/project/PostDetail';
-
+// import {Post} from './components/project/Post';
+// import About from './components/project/About';
+// import {Contact} from './components/project/Contact';
+// import Home from './components/project/Home';
+// import Layout from './components/Layout';
+// import PostLayout from './components/PostLayout';
+// import PostDetail from './components/project/PostDetail';
 
 
 export function App(){
-  // let isLogged = true ;
-  // let data = {
-  //   'st' : "This is like passing props , you can pass anything you want ."
-  // }
+  
+  let isLogged = true ;
+  let data = {
+    'st' : "This is like passing props , you can pass anything you want ."
+  }
 
   return (
     <>
 
-    {/* <Navbar />
+    <Navbar />
 
     <Routes>
 
@@ -38,34 +38,36 @@ export function App(){
       <Route  path = '/contact'  element =  {<div> I am Contact Page , you are coming from Home page </div>}/>
       <Route  path = '/about'  element  =  {<div> I am About Page . Do you want to say something about me   </div>}/>
       
-      <Route  path = '/Blog'  element  =  {<Blog />} />
-      <Route  path = '/Service'  element  =  {<Service />} />
-      <Route  path = '/Connect'  element  =  {<Connect />} /> */}
+      <Route  path = '/blog'  element  =  {<Blog />} />
+      <Route  path = '/service'  element  =  {<Service />} />
+      <Route  path = '/connect'  element  =  {<Connect />} />
 
 
 {/* Conditional Rendering Examples */}
-    {/* <Route  path = '/Login'  element  =  {<Login />} />
-    <Route  path = '/Logout'  element  =  {<Logout />} />
-    <Route  path = '/Dashboard'  element  =  {isLogged ? <Dashboard /> : <Navigate to="/Login" replace state={data}/>} />
+    <Route  path = '/login'  element  =  {<Login />} />
+    <Route  path = '/logout'  element  =  {<Logout />} />
+    <Route  path = '/dashboard'  element  =  {isLogged ? <Dashboard /> : <Navigate to="/Login" replace state={data}/>} />
 
 
 
-      <Route  path = '/Post'  element  =  {<Post />} /> */}
+      <Route  path = '/post'  element  =  {<Post />} />
 {/* if we write anything after URL in post then it show Error , For removing this error we use */}
-      {/* <Route  path = '/Post/:category'  element  =  {<Post />} />
-      <Route  path = '/Post/:category/:id'  element  =  {<Post />} /> */}
+      <Route  path = '/Post/:category'  element  =  {<Post />} />
+      <Route  path = '/Post/:category/:id'  element  =  {<Post />} />
 
 {/* For showing Error Page */}
-{/* <Route  path = '*'  element  =  {<h1> <strong>Error 404 Page not Found !!</strong></h1>}/>
+<Route  path = '*'  element  =  {<h1> <strong>OOPs Sorry , Error 404 Page not Found !!</strong></h1>}/>
 
-    </Routes>  */}
+    </Routes> 
 
 
+{/* @@@###$$$%%%^^^&&&&==>>  Creating  a  Basic  Layout  Project  using  React - Router V6  <<==@@@###$$$%%%^^^&&&&*/}
 
-   <Routes>
+
+   {/* <Routes> */}
 
 {/* For creating Nested Routes , don't use Self closed tag */}
-   <Route  path = '/'  element  =  {<Layout />} >
+   {/* <Route  path = '/'  element  =  {<Layout />} >
 
    <Route  path = 'about'  element  =  {<About />} /> 
    <Route  path = 'contact'  element  =  {<Contact />} /> 
@@ -74,16 +76,16 @@ export function App(){
    <Route  path = 'post'  element  =  {<PostLayout />} >
    <Route path = ':category'  element  =  {<PostDetail />}/>
    <Route index element  =  {<Post />} /> 
-    </Route> 
+    </Route>  */}
 
 
 {/* index shares  with " / " */}
-   <Route index element  =  {<Home />} /> 
+   {/* <Route index element  =  {<Home />} /> 
    
    </Route>
 
    <Route  path = '*'  element  =  {<h1>Error 404 Page not Found !!</h1>} />
-   </Routes>
+   </Routes> */}
 </>
 
   );
