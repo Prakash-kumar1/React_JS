c07028f00b9770c9d1e5e6e4607756842b882a21
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-15 14:22:28 +0530

    About Component (basic)

--- react-rout/src/components/project/About.js
+++ react-rout/src/components/project/About.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>This is my About page for project</div>
+    <div>This is my About page for Basic Layout Project</div>
   )
 }
