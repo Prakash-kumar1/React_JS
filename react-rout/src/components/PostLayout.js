4f42c68fabb940fb772c7185b0ae59c48d1ab448
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-15 14:24:32 +0530

    showing on Feed page

--- react-rout/src/components/PostLayout.js
+++ react-rout/src/components/PostLayout.js
@@ -1,9 +1,11 @@
+// Showing Post-Layout page inside Feed , means we have given the path of Feed
+
 import { Outlet } from "react-router-dom";
 
  const PostLayout = () =>{
     return(
         <>
-        <h2>This is Post-Layout  </h2>
+        <h2>This is Post-Layout page  </h2>
         <Outlet />
         </>
     )
