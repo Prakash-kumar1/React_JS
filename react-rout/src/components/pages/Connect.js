aa441ad23ad307486c48ac6b0ad9ee859c509cf8
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-15 13:48:41 +0530

    Connect Component (Basic Concept)

--- react-rout/src/components/pages/Connect.js
+++ react-rout/src/components/pages/Connect.js
@@ -1,7 +1,7 @@
 const Connect = () => {
     return(
         <>
-        <h1>You can connect with me on LinkedIn , Email , Twitter  and Instagram .</h1>
+        <h1>You can connect with me on Youtube ,LinkedIn , Email , Twitter  and Instagram .</h1>
         </>
     )
 } ;
