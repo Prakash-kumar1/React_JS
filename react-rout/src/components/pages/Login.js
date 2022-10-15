572d1df2415a09de7c71ec9d6e87f88b408fea44
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-15 13:52:50 +0530

    useLocation Hook ,  For Receiving the data

--- react-rout/src/components/pages/Login.js
+++ react-rout/src/components/pages/Login.js
@@ -1,5 +1,5 @@
 
-// For Receiving the data , we use ( useLocation )
+// For Receiving the data , we use ( useLocation Hook)
 import { useLocation } from "react-router-dom";
 
 const Login = () => {
@@ -11,7 +11,7 @@ const Login = () => {
         <h1>When  isLogged = false ; only then you can see this LogIn page . Navigate tag Re-direct you this
             page . this an example of conditional Rendering
         </h1>
-        <h2>{location.state.st}</h2>
+        <h3>{location.state.st}</h3>
         </>
     )
 } ;
