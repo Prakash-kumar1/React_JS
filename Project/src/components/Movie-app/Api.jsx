363b4a7306b914ab57d6cd346fe08420f2d3297f
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-19 12:58:13 +0530

    Update Api.jsx

--- Project/src/components/Movie-app/Api.jsx
+++ Project/src/components/Movie-app/Api.jsx
@@ -49,6 +49,18 @@ const Api = () =>{
         </div>
 
         <div className="container">
+            <div className="movie">
+                <div>
+                    <p>{movie1.Year}</p>
+                </div>
+                <div>
+                <img  src={movie1.Poster} alt={movie1.Title} /> 
+                </div>
+                <div>
+                <span>{movie1.Type}</span>
+                <h3>{movie1.Title}</h3>
+                </div>
+            </div>
 
         </div>
         </div>
