d9f677d4fcb00035c3260656bf2b9c45dc3ee2ef
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-19 12:19:20 +0530

    Update Api.jsx

--- Project/src/components/Movie-app/Api.jsx
+++ Project/src/components/Movie-app/Api.jsx
@@ -5,10 +5,16 @@ const API_URL = 'https://www.omdbapi.com?apikey=43b61648' ;
 const Api = () =>{
 
     const searchMovies = async(title) => {
-        const response = await fetch(``)
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json() ;
+
+        console.log(data.Search) ;
+        // console.log(data) ;
     }
 
     useEffect(() =>{
+        searchMovies('Spiderman')
+        searchMovies('Superman')
 
     }, []) ;
 
