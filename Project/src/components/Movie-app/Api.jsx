11bc8d537c71678bf84330957d19043bc6def807
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-19 12:13:01 +0530

    Create Api.jsx

---
+++ Project/src/components/Movie-app/Api.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+
+const API_URL = 'https://www.omdbapi.com?apikey=43b61648' ;
+
+const Api = () =>{
+
+    const searchMovies = async(title) => {
+        const response = await fetch(``)
+    }
+
+    useEffect(() =>{
+
+    }, []) ;
+
+
+    return(
+        <>
+        <h1>Creating a Movie App . </h1>
+        </>
+    )
+
+}
+
+
+
+
+export default Api ;
