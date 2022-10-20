a97f65c918e6c023b7436a4f0d5af8e230aae091
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-20 22:57:48 +0530

    Update Dice.jsx

--- Project/src/components/DiceGame/Dice.jsx
+++ Project/src/components/DiceGame/Dice.jsx
@@ -1,17 +1,22 @@
 import {Flex , Stack , Heading} from "@chakra-ui/layout" ;
 import {Image} from "@chakra-ui/image"  ;
 import {Button} from "@chakra-ui/button"  ;
+import { useState } from "react";
 
 export const Dice = () => {
+    const [gameStarted, setGameStarted]  =  useState(false) ;
+
+
     return (
     <Flex justify="center"  align="center">
+
 <Image  width="50%"  src= "/dices.png" />
 
 {/* Stack is like Flex , in Flex direction is Row-Wise  and In stack  direction is Column-Wise*/}
 <Stack>
-    <Heading fontSize="7x1" as="h1"> {" "} The Dice Game</Heading>
+    <Heading fontSize="9x1" as="h1"> {" "} The Dice Game</Heading>
 
-    <Button alignSelf="flex-end" bg="black" color="white" _hover={{ bg: "grey"}} >Start Game</Button>
+    <Button alignSelf="flex-end" bg="black" color="white" _hover={{ bg: "grey"}} > Start Game </Button>
 </Stack>
 
 
