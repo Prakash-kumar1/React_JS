599df25b1f2bdc8c65ff8f42447b7a4be8a0f322
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-20 22:45:47 +0530

    Update Dice.jsx

--- Project/src/components/DiceGame/Dice.jsx
+++ Project/src/components/DiceGame/Dice.jsx
@@ -1,10 +1,14 @@
-import {Flex} from "@chakra-ui/layout"
+import {Flex , Stack} from "@chakra-ui/layout"
 import {Image} from "@chakra-ui/image"
 
 export const Dice = () => {
     return <Flex>
 <Image src= "/dices.png" />
+
 {/* Stack is like Flex , in Flex direction is Row-Wise  and In stack  direction is Column-Wise*/}
+<Stack>
+    
+</Stack>
 
 
 </Flex>
