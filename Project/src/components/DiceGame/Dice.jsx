2c205ca366bec962399e73806fc9bbff2fffc2ff
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-20 22:54:45 +0530

    Update Dice.jsx

--- Project/src/components/DiceGame/Dice.jsx
+++ Project/src/components/DiceGame/Dice.jsx
@@ -10,8 +10,8 @@ export const Dice = () => {
 {/* Stack is like Flex , in Flex direction is Row-Wise  and In stack  direction is Column-Wise*/}
 <Stack>
     <Heading fontSize="7x1" as="h1"> {" "} The Dice Game</Heading>
-    
-    <Button>Start Game</Button>
+
+    <Button alignSelf="flex-end" bg="black" color="white" _hover={{ bg: "grey"}} >Start Game</Button>
 </Stack>
 
 
