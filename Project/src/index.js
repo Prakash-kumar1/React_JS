5ebb8b6179434cb6103f6a1658861065791b3dd7
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-20 22:29:45 +0530

    Importing  {  ChakraProvider }

--- Project/src/index.js
+++ Project/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App }  from './App';
 import { BrowserRouter } from "react-router-dom" ;
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
+
 
 import reportWebVitals from './reportWebVitals';
 
@@ -11,7 +14,12 @@ root.render(
   <React.StrictMode>
 
   <BrowserRouter>
+
+  <ChakraProvider>
 <App />  
+
+</ChakraProvider>
+
  </BrowserRouter>
 
   </React.StrictMode>
