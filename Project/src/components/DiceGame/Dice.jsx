580d737279cbf460dc5ddc340542829589f7e1cb
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-20 23:07:00 +0530

    Update Dice.jsx

--- Project/src/components/DiceGame/Dice.jsx
+++ Project/src/components/DiceGame/Dice.jsx
@@ -4,28 +4,36 @@ import {Button} from "@chakra-ui/button"  ;
 import { useState } from "react";
 
 export const Dice = () => {
-    const [gameStarted, setGameStarted]  =  useState(true) ;
+    const [gameStarted, setGameStarted]  =  useState(false) ;
+
+
+const startGameHandler = () =>{
+    setGameStarted(true) ;
+} ;
 
 
     return (
         <>
         {gameStarted ? (
             <Heading>Hi Everyone</Heading>
-        ):(
-                <Flex justify="center"  align="center">
 
-                <Image  width="50%"  src= "/dices.png" />
+    ):(
+
+            <Flex justify="center"  align="center">
+
+            <Image  width="50%"  src= "/dices.png" />
                 
-                {/* Stack is like Flex , in Flex direction is Row-Wise  and In stack  direction is Column-Wise*/}
-                <Stack>
-                    <Heading fontSize="9x1" as="h1"> {" "} The Dice Game</Heading>
+            {/* Stack is like Flex , in Flex direction is Row-Wise  and In stack  direction is Column-Wise*/}
+            <Stack>
+                <Heading fontSize="9x1" as="h1"> {" "} The Dice Game</Heading>
                 
-                    <Button alignSelf="flex-end" bg="black" color="white" _hover={{ bg: "grey"}} > Start Game </Button>
-                </Stack>
+                <Button alignSelf="flex-end" bg="black" color="white" _hover={{ bg: "grey"}} onClick={startGameHandler}> 
+                Start Game </Button>
+            </Stack>
                 
                 
-                </Flex>
-        )}
+            </Flex>
+    )}
         </>
 
 
