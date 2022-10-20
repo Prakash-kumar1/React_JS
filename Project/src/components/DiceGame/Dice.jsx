936dc989d84fa6e42d1072d5c976baa5da149d27
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-20 23:00:19 +0530

    Update Dice.jsx

--- Project/src/components/DiceGame/Dice.jsx
+++ Project/src/components/DiceGame/Dice.jsx
@@ -8,18 +8,24 @@ export const Dice = () => {
 
 
     return (
-    <Flex justify="center"  align="center">
+        <>
+        {gameStarted && (
+                <Flex justify="center"  align="center">
 
-<Image  width="50%"  src= "/dices.png" />
+                <Image  width="50%"  src= "/dices.png" />
+                
+                {/* Stack is like Flex , in Flex direction is Row-Wise  and In stack  direction is Column-Wise*/}
+                <Stack>
+                    <Heading fontSize="9x1" as="h1"> {" "} The Dice Game</Heading>
+                
+                    <Button alignSelf="flex-end" bg="black" color="white" _hover={{ bg: "grey"}} > Start Game </Button>
+                </Stack>
+                
+                
+                </Flex>
+        )}
+        </>
 
-{/* Stack is like Flex , in Flex direction is Row-Wise  and In stack  direction is Column-Wise*/}
-<Stack>
-    <Heading fontSize="9x1" as="h1"> {" "} The Dice Game</Heading>
 
-    <Button alignSelf="flex-end" bg="black" color="white" _hover={{ bg: "grey"}} > Start Game </Button>
-</Stack>
-
-
-</Flex>
     )
 }
