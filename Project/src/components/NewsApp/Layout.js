9e7878010ee7943de332a7146adfe9691ce1cecb
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-21 12:37:55 +0530

    Layout

---
+++ Project/src/components/NewsApp/Layout.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Outlet} from "react-router-dom";
+// import '../App.css'
+import Navbar from "./navbar";
+// import { useState } from "react";
+
+const Layout = () => {
+
+    return (
+       
+        <>
+         
+            <div id="Layout_Container">
+                <div>
+                    <h1><i class="fa fa-newspaper-o"></i>अपनी खबर</h1>
+                   
+                </div>
+
+                <Navbar />
+            </div>
+            <Outlet />
+        </>
+    )
+}
+export default Layout;
