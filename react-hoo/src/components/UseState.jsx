a97991140f5adcc5ad14e0c03429e35a98ec317c
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-10-26 22:49:58 +0530

    Update UseState.jsx

--- react-hoo/src/components/UseState.jsx
+++ react-hoo/src/components/UseState.jsx
@@ -15,7 +15,6 @@
  //              const [subject , setSubject] = useState( { sub : "Math"} ) ;     
 
 // The First item name is the " Current value " .
-
 // The Second item  setName is a function that let us update it .
 
 
