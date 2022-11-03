d614d1dfd82201b3d571adf8988f244f0630bfb9
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-03 22:53:12 +0530

    Test Cases  for  Greet File

--- testing/src/components/greet/Greet.test.js
+++ testing/src/components/greet/Greet.test.js
@@ -1,18 +1,27 @@
 import { Greet } from "./Greet";
 import { render, screen } from "@testing-library/react";
 
+
 describe("Greet", () => {
   test("renders properly", () => {
     render(<Greet />);
+    // const text = screen.getByText("Hello");          //By Default
     const text = screen.getByText("Guest");
     expect(text).toBeInTheDocument();
   });
 });
 
+// using  .only  we can run a  Single  command .  
+// using  .skip  we can Skip a  command .  
+
 describe("Greet with name", () => {
   test("renders properly with name", () => {
     render(<Greet name={"Air campus"} />);
     const text = screen.getByText("Hello Air campus");
-    expect(text).toBeInTheDocument();
+    expect(text).toBeInTheDocument();  
+// expect is called "Assert Statement"
   });
 });
+
+
+
