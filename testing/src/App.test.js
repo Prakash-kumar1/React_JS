219a680697c50631f3f5464ddc1776c860acff62
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-03 18:01:53 +0530

    Testing Basic Definitions

--- testing/src/App.test.js
+++ testing/src/App.test.js
@@ -10,8 +10,8 @@ test('renders learn react link', () => {
 // There are 3 types of testing :-
 // (1) Unit testing => Each Component / function working properly or not .
 // (2) Integration testing => Combining  more component and checking them .
-// (3) End to end (e2e) testing => 
+// (3) End to end (e2e) testing => From opening Closing means checking Completely .
 
 
-
+// For testing we use a test library  " Jest "
 
