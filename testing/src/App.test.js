09be5df2687a414b868fd0ec58e7c6a3c000a5ad
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-03 18:49:39 +0530

    Testing  Basic  Definitions

--- testing/src/App.test.js
+++ testing/src/App.test.js
@@ -1,5 +1,8 @@
+// Staring  of  31 October  and  1 November  class 
+
 import { render, screen } from '@testing-library/react';
-// Render  method is for rendering the component 
+// render => render method is for rendering the component .
+// screen =>  In Screen , only my component get rendered .
 import App from './App';
 
 test('renders learn react link', () => {
@@ -7,6 +10,8 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+// toBeInTheDocument() is a " Matching functions " . there are other Matching functions also present . 
+
 
 // There are 3 types of testing :-
 // (1) Unit testing => Each Component / function working properly or not .
@@ -17,7 +22,25 @@ test('renders learn react link', () => {
 // For testing we use a test library  " Jest "
 
 // For creating testing files we use 3 types of Extensions :
-// (1) .test.js  =>  Most times we used only this
+// (1)  .test.js  =>  Most times we used only this
 // (2)  .spec.js             (3) __Testing_module__
+
+
+
+
+// Test  Driven  Development :  it is also known as  TDD 
+// (1) => we will create all possible senario
+// (1) => we will write all Test Cases First .
+// (3) =>  Build  Our  Product .
+
+
+
+
+
+
+
+
+
+
 
 
