939558747a46d498403995309da2ad8836b01cd9
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-03 20:57:37 +0530

    Counter file

---
+++ testing/src/components/counter/Counter.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./counter.css";
+
+export function Counter() {
+  return (
+    <div>
+      <h1 className="counter_title">Counter , for styling using Block element Modifier . </h1>
+    </div>
+  );
+}
