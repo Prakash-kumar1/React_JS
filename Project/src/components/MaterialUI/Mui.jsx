0f94cc772488b836dacb931e66e9659b069bd5ac
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-06 19:06:12 +0530

    Update Mui.jsx

--- Project/src/components/MaterialUI/Mui.jsx
+++ Project/src/components/MaterialUI/Mui.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import * as React from 'react';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 
-function Mui() {
+export default function Mui() {
   return (
-<>
-    <h1>Material UI designs</h1>
+    <ButtonGroup variant="contained" aria-label="outlined primary button group">
+      <Button>One</Button>
+      <Button>Two</Button>
+      <Button>Three</Button>
+    </ButtonGroup>
+  );
+}
+
+// import React from 'react'
+// import Button from '@mui/material/Button';
+// import ButtonGroup from '@mui/material/ButtonGroup';
+
+// function Mui() {
+//   return (
+// <>
+//     <h1>Material UI designs</h1>
     {/* <ButtonGroup variant="contained" aria-label="outlined primary button group">
       <Button>One</Button>
       <Button>Two</Button>
       <Button>Three</Button>
     </ButtonGroup> */}
-</>
-  )
-}
+// </>
+//   )
+// }
 
-export default Mui
+// export default Mui
+
