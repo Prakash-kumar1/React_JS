cdd97a7cfba67d858ba8c2e4b5ce75fe6f3f2f2a
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-06 19:46:44 +0530

    Create Intro.jsx

---
+++ Project/src/components/MaterialUI/Intro.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button,Avatar } from '@mui/material';
+import { pink } from "@mui/material/colors";
+
+const Intro=()=>{
+    return(
+        <>
+            <div>
+                <h1>hello Prakash Kumar</h1>
+                {/* <Button variant="contained">Contained</Button>
+                <Avatar sx={{ bgcolor: pink[500] }}>OP</Avatar> */}
+            </div>
+        </>
+    )
+}
+export default Intro ;
