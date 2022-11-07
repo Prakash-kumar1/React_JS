e2555ac3ef3da84eb1ea3cffcdc1979beddfc14c
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-07 14:09:28 +0530

    Practicing Checkboxes and Button from material UI

--- materialui/src/components/Material.jsx
+++ materialui/src/components/Material.jsx
@@ -1,31 +1,41 @@
-// import React from 'react';
-// import Stack from '@mui/material/Stack';
-// import Button from '@mui/material/Button';
-
-// export function Material() {
-//     return (
-//         <>
-//             <Stack spacing={2} direction="row">
-//                 <button variant="text">Text</button>
-//                 <Button variant="contained">Contained</Button>
-//                 <Button variant="outlined">Outlined</Button>
-//             </Stack>
-//         </>
-//     )
-// }
-
-import  React from 'react';
+import React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import Checkbox from '@mui/material/Checkbox';
-
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 export default function Material() {
-  return (
-    <div>
+    return (
+        <>
+            <Stack spacing={2} direction="row">
+                <button variant="text">Text</button>
+                <Button variant="contained">Contained</Button>
+                <Button variant="outlined">Outlined</Button>
+            </Stack>
+
+            <div>
       <Checkbox {...label} defaultChecked />
       <Checkbox {...label} />
       <Checkbox {...label} disabled />
       <Checkbox {...label} disabled checked />
     </div>
-  );
+
+        </>
+    )
 }
+
+// import  React from 'react';
+// import Checkbox from '@mui/material/Checkbox';
+
+// const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
+// export default function Material() {
+//   return (
+//     <div>
+//       <Checkbox {...label} defaultChecked />
+//       <Checkbox {...label} />
+//       <Checkbox {...label} disabled />
+//       <Checkbox {...label} disabled checked />
+//     </div>
+//   );
+// }
