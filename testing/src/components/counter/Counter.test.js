fbfdd8d2b614231b47302d7b926c3b342d3a5348
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-09 12:10:27 +0530

    Counter Testing  all  Methods

---
+++ testing/src/components/counter/Counter.test.js
@@ -0,0 +1,74 @@
+import { render, screen } from "@testing-library/react";
+
+import { Counter } from "./Counter";
+
+describe("Counter Component", () => {
+  test("renders input correctly", () => {
+    render(<Counter />);
+
+    const compHeadingElement = screen.getByRole("heading", {
+      level: 2,
+      name: "Air campus", // text inside the heading component
+    });
+    expect(compHeadingElement).toBeInTheDocument();
+
+    const testingHeadingElement = screen.getByRole("heading", {
+      level: 3,
+    });
+    expect(testingHeadingElement).toBeInTheDocument();
+
+    const nameElement = screen.getByRole("textbox", {
+      name: "Name",
+    });
+    expect(nameElement).toBeInTheDocument();
+
+    const ageElement = screen.getByRole("textbox", {
+      name: "age",
+    });
+    expect(ageElement).toBeInTheDocument();
+
+    const locationElement = screen.getByRole("combobox");
+    expect(locationElement).toBeInTheDocument();
+
+    const termsElement = screen.getByRole("checkbox");
+    expect(termsElement).toBeInTheDocument();
+
+    const submitBtnElement = screen.getByRole("button", {
+      name: "submit",
+    });
+    expect(submitBtnElement).toBeInTheDocument();
+
+    // getByLabelText
+    const nameElement2 = screen.getByLabelText("Name", {
+      selector: "input",
+    });
+    expect(nameElement2).toBeInTheDocument();
+
+    const locationElement2 = screen.getByLabelText("Location");
+    expect(locationElement2).toBeInTheDocument();
+
+    // getByPlaceholderText
+    const nameElement3 = screen.getByPlaceholderText("firstName");
+    expect(nameElement3).toBeInTheDocument();
+
+    // getByText -> p, span, div
+    const paraElement = screen.getByText("good night. thank you");
+    expect(paraElement).toBeInTheDocument();
+
+    // getByDisplayValue
+    const nameElement4 = screen.getByDisplayValue("vasanth");
+    expect(nameElement4).toBeInTheDocument();
+
+    // getByAltText
+    const imageElement = screen.getByAltText("picture");
+    expect(imageElement).toBeInTheDocument();
+
+    // getByTitle
+    const paraElement2 = screen.getByTitle("vasanth");
+    expect(paraElement2).toBeInTheDocument();
+
+    // custom -> test-id
+    const customElement = screen.getByTestId("custom");
+    expect(customElement).toBeInTheDocument();
+  });
+});
