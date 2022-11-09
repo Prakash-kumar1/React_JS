fb7612ee9e6fa01abc2f931c13f8fc1822233c44
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-09 12:36:24 +0530

    Update App.js

--- testing/src/App.js
+++ testing/src/App.js
@@ -6,8 +6,8 @@ export function App() {
   let  skills = ["HTML","CSS","JavaScript","Bootstrap","React-JS"]
   return (
     <div className="App">
-      <Counter />
-      <Greet />
+      {/* <Counter />
+      <Greet /> */}
       <Skills skill={skills}/>
     </div>
   );
