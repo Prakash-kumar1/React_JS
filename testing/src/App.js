e669bb3f7a3f929f1a4b3561afc40ae0909822d3
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-09 14:22:21 +0530

    Importing  All  Testing files

--- testing/src/App.js
+++ testing/src/App.js
@@ -1,19 +1,19 @@
-// import { Counter } from "./components/counter/Counter";
-// import { Greet } from "./components/greet/Greet";
+// Read about Ant Design (https://ant.design/docs/react/introduce)  and   testing library (https://testing-library.com/docs/)
+
+import { Counter } from "./components/counter/Counter";
+import { Greet } from "./components/greet/Greet";
 import {Skills} from "./components/skills/Skills";
 
 export function App() {
 const  skills = ["HTML","CSS","JavaScript"]
   return (
     <div className="App">
-      {/* <Counter />
-      <Greet /> */}
+      <Counter />
+      <Greet />
       <Skills skills={skills}/>
     </div>
   );
 }
-
-
 
 
 
