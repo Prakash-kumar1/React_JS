e138993264ddd59a1502bbfa9776843025217ccf
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-09 12:08:17 +0530

    Update Counter.jsx

--- testing/src/components/counter/Counter.jsx
+++ testing/src/components/counter/Counter.jsx
@@ -1,10 +1,61 @@
+// import React from "react";
+// import "./counter.css";
+
+// export function Counter() {
+//   return (
+//     <div>
+//       <h1 className="counter_title">Counter , for styling using Block element Modifier . </h1>
+//     </div>
+//   );
+// }
+
 import React from "react";
+import { Button } from "antd";
+import { PlayCircleOutlined } from "@ant-design/icons";
 import "./counter.css";
 
 export function Counter() {
   return (
-    <div>
-      <h1 className="counter_title">Counter , for styling using Block element Modifier . </h1>
-    </div>
+    <>
+      <Button type="ghost" size="small">
+        <PlayCircleOutlined /> test button
+      </Button>
+      <h2>Air campus</h2>
+      <h3>react testing class</h3>
+      <p title="vasanth">good night. thank you</p>
+      <img src="" alt="picture" />
+      <div data-testid="custom"></div>
+      <form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            placeholder="firstName"
+            value={"vasanth"}
+          />
+        </div>
+        <div>
+          <label htmlFor="age">age</label>
+          <textarea id="age"></textarea>
+        </div>
+        <div>
+          <label htmlFor="location">Location</label>
+          <select defaultValue={"uk"} id="location">
+            <option value="us">US</option>
+            <option value="uk">UK</option>
+            <option value="ind">India</option>
+            <option value="rsa">Russia</option>
+          </select>
+        </div>
+
+        <div>
+          <label>
+            <input type="checkbox" id="terms" /> I agree to the above terms
+          </label>
+        </div>
+        <button>submit</button>
+      </form>
+    </>
   );
 }
