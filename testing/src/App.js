1056f5736d55d2ebaf54fcee6a353e082d4e8eed
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-11 19:47:15 +0530

    Update App.js

--- testing/src/App.js
+++ testing/src/App.js
@@ -1,16 +1,18 @@
 // Read about Ant Design (https://ant.design/docs/react/introduce)  and   testing library (https://testing-library.com/docs/)
 
-import { Counter } from "./components/counter/Counter";
-import { Greet } from "./components/greet/Greet";
-import {Skills} from "./components/skills/Skills";
+// import { Counter } from "./components/counter/Counter";
+// import { Greet } from "./components/greet/Greet";
+// import {Skills} from "./components/skills/Skills";
+import Counter2 from "./components/counter2/Counter2";
 
 export function App() {
-const  skills = ["HTML","CSS","JavaScript"]
+// const  skills = ["HTML","CSS","JavaScript"] ;
   return (
     <div className="App">
-      <Counter />
+      {/* <Counter />
       <Greet />
-      <Skills skills={skills}/>
+      <Skills skills={skills}/> */}
+      <Counter2 />
     </div>
   );
 }
