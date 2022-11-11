f73f26f1ba666b63e48fa96e28b24c6db84e6c36
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-11 11:41:46 +0530

    Practicing  Material UI

--- materialui/src/components/Material.jsx
+++ materialui/src/components/Material.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+
+
+// import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import NavigationIcon from '@mui/icons-material/Navigation';
+
+
 import Checkbox from '@mui/material/Checkbox';
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
 
 export default function Material() {
     return (
@@ -13,13 +24,32 @@ export default function Material() {
                 <Button variant="outlined">Outlined</Button>
             </Stack>
 
-            <div>
+    <div>
       <Checkbox {...label} defaultChecked />
       <Checkbox {...label} />
       <Checkbox {...label} disabled />
       <Checkbox {...label} disabled checked />
     </div>
 
+<Button color="secondary">Secondary</Button>
+<Button variant="contained" color="success">Success</Button>
+<Button variant="outlined" color="error">Error</Button>
+
+{/* <Box sx={{ '& > :not(style)': { m: 1 } }}> */}
+      <Fab color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
+      <Fab color="secondary" aria-label="edit">
+        <EditIcon />
+      </Fab>
+      <Fab variant="extended">
+        <NavigationIcon sx={{ mr: 1 }} />
+        Navigate
+      </Fab>
+      <Fab disabled aria-label="like">
+        <FavoriteIcon />
+      </Fab>
+{/* </Box> */}
         </>
     )
 }
