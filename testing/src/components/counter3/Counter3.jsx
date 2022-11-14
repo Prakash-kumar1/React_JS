7a2aa24e860afef09d4accd8725d58472421dedc
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-14 19:53:24 +0530

    Update Counter3.jsx

---
+++ testing/src/components/counter3/Counter3.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+
+export function Counter3() {
+    const[count , setCount] = useState(0) ;
+    const[inputVal, setInputVal] = useState(0) ;
+
+
+
+  return (
+    <div>
+        <h1>{count}</h1>
+        {/* <button onClick={() => setCount(count+1)}>Add</button> */}
+        {/* <button onClick={() => setCount(count-1)}>Subtract</button> */}
+        <button onClick={() => setCount(prev => (prev+1))}>Add</button>
+        <button onClick={() => setCount(prev => (prev-1))}>Subtract</button>
+
+        <input type="number" name="inputVal" value={inputVal}
+        onChange={(e)=> setInputVal(parseInt(e.target.value))}/>
+    
+    <button onClick= {()=> setCount(inputVal)}> setCount </button>
+    </div>
+  )
+}
