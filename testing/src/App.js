15fcd45094821cd10e3fcb15262c0fdb59eb6352
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-15 10:38:58 +0530

    Update App.js

--- testing/src/App.js
+++ testing/src/App.js
@@ -1,18 +1,32 @@
 // Read about Ant Design (https://ant.design/docs/react/introduce)  and   testing library (https://testing-library.com/docs/)
 
-import { Counter } from "./components/counter/Counter";
-import { Greet } from "./components/greet/Greet";
-import {Skills} from "./components/skills/Skills";
-import {Counter2} from "./components/counter2/Counter2";
+// import { Counter } from "./components/counter/Counter";
+// import { Greet } from "./components/greet/Greet";
+// import {Skills} from "./components/skills/Skills";
+// import {Counter2} from "./components/counter2/Counter2";
+import { useState } from "react";
+import { Counter3 } from "./components/counter3/Counter3";
 
 export function App() {
 // const  skills = ["HTML","CSS","JavaScript"] ;
+const [count, setCount] = useState(0) ;
+
+const handleIncrement = () => {
+setCount((prev) => prev + 1) ;
+} ;
+
+const handleDecrement = () => {
+setCount((prev) => prev - 1) ;
+} ;
+
+
   return (
     <div className="App">
-      <Counter />
+      {/* <Counter />
       <Greet />
       <Skills skills={skills}/>
-      <Counter2 />
+      <Counter2 /> */}
+      <Counter3 count={count} handleIncrement = {handleIncrement}  handleDecrement = {handleDecrement} />
     </div>
   );
 }
