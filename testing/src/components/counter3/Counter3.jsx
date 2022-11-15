31ac5bf4a8ec80690ceced4eda4facf0ebae0fbc
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-15 11:03:18 +0530

    testing

--- testing/src/components/counter3/Counter3.jsx
+++ testing/src/components/counter3/Counter3.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 
 
-export function Counter3() {
+export function Counter3(props) {
    
   return (
     <div>
         <h1>Counter-3</h1>
-        <h1>{props.count}</h1>
+        <h2>{props.count}</h2>
 
         {props.handleIncrement && (
  <button onClick={props.handleIncrement}>increment</button>
---
+++ testing/src/components/counter3/Counter3.test.js
@@ -0,0 +1,65 @@
+import { Counter3 } from "./Counter3";
+import { render, screen} from "@testing-library/react" ;
+import user from "@testing-library/user-event" ;
+
+describe('Counter 3', () => {
+    test('renders properly' , () =>{
+        render(<Counter3 count={10} />) ;
+        const countElement = screen.getByRole("heading" , {
+            level: 2,
+        }) ;
+        expect(countElement).toHaveTextContent("10") ;
+
+        const headingElement = screen.getByText("Counter-3") ;
+        expect(countElement).toBeInTheDocument() ;
+    }) ;
+
+    test('renders properly with handlers', () =>{
+        const handleIncrement = jest.fn() ;
+        const handleDecrement = jest.fn() ;
+
+        render(
+            <Counter3 count={0} 
+            handleIncrement = {handleIncrement}  
+            handleDecrement = {handleDecrement} />
+        ) ;
+
+        const incrementBtn = screen.getByRole('button' , {
+            name: "increment"
+        }) ;
+         
+        const decrementBtn = screen.getByRole('button' , {
+            name: "decrement"
+        }) ;
+
+        user.click(incrementBtn) ;
+        user.click(decrementBtn) ;
+
+        expect(handleIncrement).toHaveBeenCalledTimes(1) ;
+        expect(handleDecrement).toHaveBeenCalledTimes(1) ;
+
+    }) ;
+
+}) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
