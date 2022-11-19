17f0186500d211d28c9dc2f7e4fdd43dbb2ff9f8
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-19 11:43:41 +0530

    Material UI Project [Pending]

--- materialui/src/App.js
+++ materialui/src/App.js
@@ -1,12 +1,10 @@
-import Donutchart from "./components/Donutchart";
-// import  Material  from "./components/Material";
 
 
 function App() {
   return (
     <div>
-      {/* <Material /> */}
-      <Donutchart />
+ 
+      
     </div>
   );
 }
