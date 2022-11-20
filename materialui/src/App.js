7e49935147bf8c3518c394835e0e0225cb4a85d1
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-20 13:31:20 +0530

    Material UI Project completed

--- materialui/src/App.js
+++ materialui/src/App.js
@@ -1,23 +1,33 @@
 import {Feed} from "./components/Feed";
 import Rightbar from "./components/Rightbar";
 import {Sidebar} from "./components/Sidebar";
-import {Box, Stack} from "@mui/material" ;
+import {Box, createTheme, Stack, ThemeProvider} from "@mui/material" ;
 import {Navbar} from "./components/Navbar";
+import Add from "./components/Add";
+import { useState } from "react";
 
 
  function App()  {
+  const [mode, setMode] = useState("light")
+
+  const darkTheme = createTheme({
+    palette:{
+      mode: mode
+    }
+  })
+
   return (
-    <div>
-      <Box>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
         <Navbar/>
         <Stack direction="row" spacing={2} justifyContent="space-between">
-      <Sidebar />
+      <Sidebar setMode={setMode} mode={mode}/>
       <Feed />
       <Rightbar />
         </Stack>
-          
+          <Add/>
       </Box>
-    </div>
+    </ThemeProvider>
   );
 }
 
