f35487cf5748a68ca31945f17c0b0dbc42ca1271
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-24 18:10:57 +0530

    Redux basic Definition

--- redux/src/App.js
+++ redux/src/App.js
@@ -2,8 +2,25 @@ import './App.css';
 
 // There are 4 main part in Redux
 // store => Globalized State (there is every data and states is present).
+
 // Actions => like Increment , decrement and anything 
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
 // Reducers => Check which action is performing and modify and Update the store .
+const counter = (state = 0, action) => {
+  switch(action.type){
+    case 'INCREMENT':
+      return state + 1 ;
+    case 'DECREMENT':
+      return state - 1 ;
+    default :
+      return state ;
+  }
+}
+
 // Dispatch => Execute action
 
 function App() {
