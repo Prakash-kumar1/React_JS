b6111e2d6d2df12bdbd58bf664c4de2cc3a3cf0c
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-24 17:48:42 +0530

    Redux  Definition

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,5 +1,10 @@
-
 import './App.css';
+
+// There are 4 main part in Redux
+// store => Globalized State (there is every data and states is present).
+// Actions => like Increment , decrement and anything 
+// Reducers => Check which action is performing and modify and Update the store .
+// Dispatch => Execute action
 
 function App() {
   return (
