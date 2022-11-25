1001c11c336e3989bd80551d5f750a283dd8900e
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-25 18:24:38 +0530

    Update index.js

---
+++ redux/src/store/index.js
@@ -0,0 +1,22 @@
+import {createStore} from 'redux' ;
+
+const DEFAULT_STATE = {
+    counter: 10,
+    name: "Prakash",
+    age: 22,
+    city: "Obra",
+    mobile: [],
+    desktop: [
+        {
+            make: "MacBook-Pro"
+        } ,
+        {
+            make: "MacBook-Pro"
+        } ,
+        {
+            make: "MacBook-Pro"
+        } 
+    ]
+}
+
+
