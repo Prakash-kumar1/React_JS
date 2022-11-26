ba58c53b91d36ed1076a58a502fb0bc1a6fc7bc3
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-26 12:21:09 +0530

    Craeting a Counter in Redux

--- redux/src/components/Counter.jsx
+++ redux/src/components/Counter.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { useSelector } from 'react-redux' ;
+import { useSelector, useDispatch } from 'react-redux' ;
 
 const Counter = () => {
 
@@ -7,16 +6,27 @@ const Counter = () => {
     // useSelector - it takes in the global state and 
     //                selects the counter state from it .
 
+    const prakashDispatch = useDispatch() ;
+
     const counter = useSelector((state) => {
         return state.counter
     })
+
+    const handleIncrement = () => {
+        prakashDispatch({"type":"increment", "payload": {"step": 5}})
+        // parakashDispatch({"type":"increment", "payload": {"steps": 5}})
+    }
+
+    const handleDecrement = () => {
+        prakashDispatch({"type":"decrement", "payload": {"step": 10}})
+    }
 
   return (
     <main style={{"textAlign":"center"}}>
         <h1>{counter}</h1>
 
-        <button style={{"margin":"20px"}}>Increment</button>
-        <button style={{"margin":"20px"}}>Decrement</button>
+        <button style={{"margin":"20px"}} onClick={handleIncrement}>Increment</button>
+        <button style={{"margin":"20px"}} onClick={handleDecrement}>Decrement</button>
 
     </main>
   ) ;
