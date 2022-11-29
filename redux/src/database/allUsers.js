dca8233d52d9176790758dc602874de9c805bbe9
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-29 20:22:10 +0530

    Creating  a  Dummy Data of All users

---
+++ redux/src/database/allUsers.js
@@ -0,0 +1,84 @@
+export default allUsers = [
+    {
+        "email" : "kumarsanju4881@gmail.com" ,
+        "password" : "dragon"
+    } ,
+    {
+        "email" : "prakashKr4881@gmail.com" ,
+        "password" : "Light"
+    } ,
+    {
+        "email" : "govindverma.gkv740@gmail.com" ,
+        "password" : "Govind"
+    } ,
+    {
+        "email" : "kolapkarabhishek1@gmail.com" ,
+        "password" : "Abhi"
+    } ,
+    {
+        "email" : "sadhna.khairia@gmail.com" ,
+        "password" : "meditation"
+    } ,
+    {
+        "email" : "puranchandra02@gmail.com" ,
+        "password" : "Lohar"
+    } ,
+    {
+        "email" : "kumarsanju4881@gmail.com" ,
+        "password" : "dragon"
+    } ,
+    {
+        "email" : "kumarsanju4881@gmail.com" ,
+        "password" : "dragon"
+    } ,
+    {
+        "email" : "kumarsanju4881@gmail.com" ,
+        "password" : "dragon"
+    } ,
+    {
+        "email" : "kumarsanju4881@gmail.com" ,
+        "password" : "dragon"
+    } ,
+    {
+        "email" : "kumarsanju4881@gmail.com" ,
+        "password" : "dragon"
+    } ,
+    {
+        "email" : "kumarsanju4881@gmail.com" ,
+        "password" : "dragon"
+    } ,
+    {
+        "email" : "kumarsanju4881@gmail.com" ,
+        "password" : "dragon"
+    } ,
+    {
+        "email" : "kumarsanju4881@gmail.com" ,
+        "password" : "dragon"
+    } 
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
