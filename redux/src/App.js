153c6a544df5cc17764140b33d869591d356ef85
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-29 21:52:20 +0530

    Importing counter and  Authentication  project file

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,5 +1,10 @@
 // import Counter from "./components/Counter";
 
+import {useSelector} from 'react-redux';
+import Dashboard from "./components/Dashboard";
+import Home from "./components/Home";
+import { Login } from "./components/LoginForm";
+
 // Creating a Log In  &&  LogOut (Authentication) in Redux :-
 
 // Steps :=> (1) List of Registered Users .
@@ -11,9 +16,25 @@
 
 
 function App() {
+
+  const loggedInUser = useSelector((state) => {
+    return state.loggedInUser
+  })
+
+
   return (
     <>
+
     {/* <Counter/> */}
+    
+    {loggedInUser ? 
+      
+      <Dashboard /> : 
+      
+      <> <Home />
+        <Login />
+      </>}
+
     </>
   );
 }
