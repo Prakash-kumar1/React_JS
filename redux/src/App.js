065b43b9c31101b9dde38ad4fd7f40786ad11aad
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-29 19:52:03 +0530

    Starting Revision of today Redux class

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,10 +1,19 @@
-import Counter from "./components/Counter";
+// import Counter from "./components/Counter";
+
+// Creating a Log In  &&  LogOut (Authentication) in Redux :-
+
+// Steps :=> (1) List of Registered Users .
+//           (2) LogIn form - email , password
+//           (3) once logged in , redirect to the Dashboard
+//           (4) if not Logged in , redirect to the Home Page
+
+// requirements :-  Logged in user information should be saved in Redux .
 
 
 function App() {
   return (
     <>
-    <Counter/>
+    {/* <Counter/> */}
     </>
   );
 }
