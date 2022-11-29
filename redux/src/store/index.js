28589adb67447f5028e9e08a6fc49802e048af33
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-11-29 21:51:34 +0530

    Code for  Authentication Project  and  Counter

--- redux/src/store/index.js
+++ redux/src/store/index.js
@@ -1,50 +1,70 @@
 import {createStore} from 'redux' ;
 
-const DEFAULT_STATE = {
-    counter: 100,
-    name: "Prakash",
-    age: 22,
-    city: "Obra",
-    mobile: [],
-    desktop: [
-        {
-            make: "MacBook-Pro"
-        } ,
-        {
-            make: "MacBook-Pro"
-        } ,
-        {
-            make: "MacBook-Pro"
-        } 
-    ]
-}
-
-// action = {
-//     "type" : "increment" ,
-//     "payload" : {
-//         "step" : 5
-//     }
+// const DEFAULT_STATE = {
+//     counter: 100,
+//     name: "Prakash",
+//     age: 22,
+//     city: "Obra",
+//     mobile: [],
+//     desktop: [
+//         {
+//             make: "MacBook-Pro"
+//         } ,
+//         {
+//             make: "MacBook-Pro"
+//         } ,
+//         {
+//             make: "MacBook-Pro"
+//         } 
+//     ]
 // }
 
-const counterReducer = (state = DEFAULT_STATE, action) => {
-    console.log("action", action) ;
+// // action = {
+// //     "type" : "increment" ,
+// //     "payload" : {
+// //         "step" : 5
+// //     }
+// // }
 
-    if (action.type === 'increment'){
-        console.log("Step Increases", action.payload.step)
-        // return {...state, counter: state.counter + 1}
-    // OR
-        return {...state, counter: state.counter + action.payload.step} 
-    }
-    else if (action.type === 'decrement'){
-        console.log("Step decreases", action.payload.step)
-        // return {...state, counter: state.counter - 1}
-        return {...state, counter: state.counter - action.payload.step} 
+// const counterReducer = (state = DEFAULT_STATE, action) => {
+//     console.log("action", action) ;
 
-    }
+//     if (action.type === 'increment'){
+//         console.log("Step Increases", action.payload.step)
+//         // return {...state, counter: state.counter + 1}
+//     // OR
+//         return {...state, counter: state.counter + action.payload.step} 
+//     }
+//     else if (action.type === 'decrement'){
+//         console.log("Step decreases", action.payload.step)
+//         // return {...state, counter: state.counter - 1}
+//         return {...state, counter: state.counter - action.payload.step} 
 
-    return state 
+//     }
+
+//     return state 
+// }
+
+// const store = createStore(counterReducer)
+
+
+// Creating a Log In  &&  LogOut (Authentication) in Redux :-
+
+const DEFAULT_STATE = {
+    "loggedInUser": null,
 }
 
-const store = createStore(counterReducer)
+
+const authReducer = (state = DEFAULT_STATE, action) => {
+
+    if(action.type==='SET_LOGIN_DATA'){
+        return {"loggedInUser": action.payload}
+    }
+
+    return state
+}
+
+
+const store = createStore(authReducer)
 
 export default store ;
