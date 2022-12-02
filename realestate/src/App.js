f8c0464a7cb859109ba07c98891eb3bff242c617
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-12-02 12:47:07 +0530

    importing files and using Routes

--- realestate/src/App.js
+++ realestate/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 import {Property} from "./components/Property";
 import {Header} from "./components/Header";
-// import {Page } from './components/Page';
+import {Page } from './components/Page';
 
 export const App = () => {
 
@@ -14,7 +14,7 @@ export const App = () => {
    <Routes>
         <Route path="/" element= {<Header favProperty={favProperty} setfavProperty={setfavProperty}/>} >
           <Route index element= {<Property favProperty={favProperty} setfavProperty={setfavProperty}  />} />
-          {/* <Route path="page" element= {<Page favProperty={favProperty} setfavProperty={setfavProperty} />} />  */}
+          <Route path="page" element= {<Page favProperty={favProperty} setfavProperty={setfavProperty} />} /> 
         </Route>
     </Routes>
   );
