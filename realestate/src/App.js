f8e134485be85ff8c0d12dbe89d65ebfc1d7294d
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-12-03 11:57:30 +0530

    Importing all files and routing

--- realestate/src/App.js
+++ realestate/src/App.js
@@ -1,26 +1,39 @@
 import { Routes, Route } from "react-router-dom";
-import {useState} from "react";
-import './App.css';
+import { useState } from "react";
 
-import {Property} from "./components/Property";
-import {Header} from "./components/Header";
-import {Page } from './components/Page';
+import { Property } from "./components/Property/Property";
+import { Header } from "./components/Header/Header";
+import { Page } from "./components/Page/Page";
+
+import "./App.css";
 
 export const App = () => {
-
-  const [favProperty, setfavProperty] = useState([])
+  const [favProperty, setFavProperty] = useState([]);
 
   return (
-   <Routes>
-        <Route path="/" element= {<Header favProperty={favProperty} setfavProperty={setfavProperty}/>} >
-          <Route index element= {<Property favProperty={favProperty} setfavProperty={setfavProperty}  />} />
-          <Route path="page" element= {<Page favProperty={favProperty} setfavProperty={setfavProperty} />} /> 
-        </Route>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Header favProperty={favProperty} setFavProperty={setFavProperty} />
+        }
+      >
+        <Route
+          index
+          element={
+            <Property
+              favProperty={favProperty}
+              setFavProperty={setFavProperty}
+            />
+          }
+        />
+        <Route
+          path="page"
+          element={
+            <Page favProperty={favProperty} setFavProperty={setFavProperty} />
+          }
+        />
+      </Route>
     </Routes>
   );
-
-}
-
-
-
-
+};
