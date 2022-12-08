7653b516b66870583f5f867631853516f0761fa6
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-12-08 22:36:06 +0530

    Using Counter in Coin with help of { useSelector }

--- redux/src/features/coin/Coin.js
+++ redux/src/features/coin/Coin.js
@@ -1,6 +1,12 @@
+import { useSelector } from "react-redux";
+
 export const Coin=()=> {
+  const count = useSelector((state) => state.counter.count) ;
   return (
-    <div>Coin</div>
+    <div>
+{/* using redux we can simply pass the value of count in Coin File */}
+      <h4 style={{color: "magenta"}}>Coin(Redux-Toolkit): {count}</h4>
+    </div>
   )
 }
 
