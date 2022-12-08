f71ac2cd732f818a7a72900d73072b1cf9f5eb06
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-12-08 22:37:52 +0530

    Simple Counter && Counter using Redux-Toolkit

--- redux/src/features/counter/Counter.jsx
+++ redux/src/features/counter/Counter.jsx
@@ -1,14 +1,36 @@
-
 import { useState } from "react";
 
+// !!!@@@###$$$%%%^^^&&& -->> Creating Counter using Redux-Toolkit && using in Coin <<-- !!!@@@###$$$%%%^^^&&&
+
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment, incrementByAmount, divideByAmount } from './counterSlice' ;
 
 export const Counter = () => {
-const [count, setCount] = useState(0)
+
+// !!!@@@###$$$%%%^^^&&& -->> Creating Counter using Redux-Toolkit && using in Coin <<-- !!!@@@###$$$%%%^^^&&&
+const count = useSelector((state) => state.counter.count) ;
+const dispatch = useDispatch() ;
+
+// !!!@@@###$$$%%%^^^&&& -->> Simple Counter using React <<-- !!!@@@###$$$%%%^^^&&&
+const [num, setNum] = useState(0)
+
   return (
     <div>
-    <button onClick={() => {setCount(count + 1)}}> + </button>
-    <h1>Count: {count}</h1>
-    <button onClick={() => {setCount(count - 1)}}> - </button>
+
+   <button aria-label="Increment value" onClick={() => { dispatch(increment()) }}> + </button>
+    <h1>Counter by redux-Toolkit: {count}</h1>
+    <button aria-label="Decrement value" onClick={() => { dispatch(decrement()) }}> -</button>
+
+    <button onClick={() => { dispatch(incrementByAmount(100)) }}> IncrementBy100 </button>
+    <button onClick={() => { dispatch(divideByAmount(10)) }}> divideBy10 </button>
+
+{/* // !!!@@@###$$$%%%^^^&&& -->> Simple Counter using React <<-- !!!@@@###$$$%%%^^^&&& */}
+    <h1>Counter by react: {num}</h1>
+    <button onClick={() => {setNum(num + 1)}}> + </button>
+    <button onClick={() => {setNum(num - 1)}}> - </button>
+
     </div>
   )
 }
+
+
