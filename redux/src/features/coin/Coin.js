619a52d1457b24fc128f93c195273be1f8857c9f
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-12-11 18:59:45 +0530

    Coin File using for Redux-Toolkit as a Example

--- redux/src/features/coin/Coin.js
+++ redux/src/features/coin/Coin.js
@@ -2,10 +2,12 @@ import { useSelector } from "react-redux";
 
 export const Coin=()=> {
   const count = useSelector((state) => state.counter.count) ;
+  const themeTextColor = useSelector((state) => state.theme.color) ;
+
   return (
     <div>
 {/* using redux we can simply pass the value of count in Coin File */}
-      <h4 style={{color: "magenta"}}>Coin(Redux-Toolkit): {count}</h4>
+      <h4 style={{color: themeTextColor}}>Coin(Redux-Toolkit): {count}</h4>
     </div>
   )
 }
