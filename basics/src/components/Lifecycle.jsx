876613c6ff5c0336432c0dd1f17ce590442b561b
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-12-17 22:16:15 +0530

    Lifecycle methods  and  Definitions

--- basics/src/components/Lifecycle.jsx
+++ basics/src/components/Lifecycle.jsx
@@ -49,5 +49,3 @@
 // (4) =>  Un-Mounting Phase =>  It is the final phase of the react component lifecycle. It is called when a component instance is destroyed and unmounted from the DOM. This phase contains only one method and is given below.
 
 // componentWillUnmount() => This method is invoked immediately before a component is destroyed and unmounted  permanently. It performs any necessary cleanup related task such as invalidating timers, event listener, canceling  network requests, or cleaning up DOM elements. If a component instance is unmounted, you cannot mount it again.
-
-
