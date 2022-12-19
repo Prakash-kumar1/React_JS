0ea3a0ae55cddc59718a2b54658982641e6a8361
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-12-19 12:09:48 +0530

    importing all Files

--- 6-September/src/App.js
+++ 6-September/src/App.js
@@ -1,14 +1,14 @@
 
 import './App.css';
-// import {Toggle} from './components/Toggle.js'
-// // inside curly braces toggle represents class
+import {Toggle} from './components/Toggle.js'
+// inside curly braces toggle represents class
 
-// import Button from './components/Button';
-// import Prop from './Prop';
-// import State from './State';
-// import Profile from "./Profile";
-// import Func from "./Func";
-import { Corona } from './components/Corona';
+import Button from './components/Button';
+import Prop from './Prop';
+import State from './State';
+import Profile from "./Profile";
+import Func from "./Func";
+// import { Corona } from './components/Corona';
 
 
 function App() {
@@ -16,28 +16,28 @@ function App() {
     
     <section>
 
-      < Corona />
+      {/* < Corona /> */}
 
-      {/* <Toggle name="Present in props , console" />
+      <Toggle name="Present in props , console" />
 
       <Button text="Delete" class="delete" />
       <Button text="Save" class="save" />
 
       <Profile />
-      <Func text = "Functional Component example"/> */}
+      <Func text = "Functional Component example"/>
 
 
-{/* <Prop 
+<Prop 
 
-   Passing Props in Functional component  
+  //  Passing Props in Functional component  
    text="Passing data in props" 
   obj={{name:'Rinki' , food:'Dosa' , note: 'Passing Object in two curly Braces'}} 
 
-   Passing Props in Class component  
+  //  Passing Props in Class component  
    text1="Passing data in class component always used with this keyword" 
- obj1={{name:'Prakash' , food:'Chicken' , note: 'Passing Object in two curly Braces'}}/>  */}
+ obj1={{name:'Prakash' , food:'Chicken' , note: 'Passing Object in two curly Braces'}}/> 
 
-  {/* <State />  */}
+  <State /> 
 
     </section>
   );
