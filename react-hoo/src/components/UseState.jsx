5c27de3b2c68947ffac487cbf1892c7d3415b93a
Prakash-kumar1 <kumarsanju4881@gmail.com>
2023-01-04 21:41:31 +0530

    Update UseState.jsx

--- react-hoo/src/components/UseState.jsx
+++ react-hoo/src/components/UseState.jsx
@@ -45,23 +45,19 @@ export function UseState() {
         setQuote("I am deserving a life full of Luxury.") ;
     };
 
-
     const handleIncrement = () =>{
         setCount(count + 1) ;
     }
-
 
     function changeName() {
         console.log("clicked !!")
         return setName1("Ritesh Agarhari") ;
     }
 
-
     function changeFlag() {
         console.log("Flag clicked !!")
         return setFlag(!flag) ;
     }
-
 
     const addNames = (e) => {
         e.preventDefault() ;
