776cc105279b18f3c2a26b924a0388e74afc8325
Prakash-kumar1 <kumarsanju4881@gmail.com>
2023-02-16 21:49:02 +0530

    React-JS basics && LifeCycles methods

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,6 +1,9 @@
-// React-JS Is a Javascript library and also a Dependcies. it helps to build single page application . 
+// React Introduction ==>>   React JS is a Client side Javascript library for building User Interfaces . 
+//                           It is maintained by  Facebook  and it play with  Virtual DOM. This is 
+//                           very fast due to  Virtual DOM. that's why react is more popular to other Framework 
+//                           && Library. React has component based Architecture for application Development . 
 
-// Components :=   Header , Footer , section , aside  etc  everything  is a component .
+// Components :=   Header, Footer, section, aside, Navbar  etc  everything  is a component .
 
 import logo from './logo.svg';
 import './App.css';
@@ -20,7 +23,7 @@ export function App() {
 // using <> and </> , when we write something above on  div .
     <>
     <h1> This is me , Prakash {name} </h1>
-    <p>React - JS is a Client side javascript library . ( HTML + JS ==  JSX) it is used to build User Interface (UI designs) and Single Page Applications (SPA) . Loading the page is called " Rendering " . react - Js is lightWeight , Fastest and React has Component based Architecture . React Js follow  Declartive approach (line no.- 4) . React-js creates Virtual DOM . compare to Real DOM , Virtual DOM is very lightWeight and fast . it is like internal tree data structure . </p>
+    <p>React - JS is a Client side javascript library. (HTML+JS == JSX) it is used to build User Interface (UI designs) and Single Page Applications (SPA) . Loading the page is called " Rendering " . react - Js is lightWeight , Fastest and React has Component based Architecture . React Js follow  Declartive approach (line no.- 4) . React-js creates Virtual DOM . compare to Real DOM , Virtual DOM is very lightWeight and fast . it is like internal tree data structure . </p>
     
     <div className="ist">Lovely</div>
     <img src="C:\Users\Monika Priya Singh\OneDrive\Pictures\Screenshots\github.jpg" alt="nothing Image found" />
@@ -28,16 +31,8 @@ export function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/* Edit <code>src/App.js</code> and save to reload. */}
-          Today we are starting React with Vasanth sir .
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <p>Today we are starting React with Vasanth sir .</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React with Prakash Kumar
         </a>
       </header>
@@ -67,21 +62,16 @@ export function App2(){
   return <h2>app2 is a second function </h2>
 }
 
-// React Introduction ==>>   React (also known as React.js or ReactJS)  is a Javascript library for building User
-//                           Interfaces . It is maintained by  Facebook  and it play with  Virtual DOM . This is 
-//                           very fast due to  Virtual DOM . that's why react is more popular to other Framework 
-//                           and  Library . React has component based Architecture for application Development . 
-
 
 // state :=>  state is current Information or data of the component .
 
 
 // Node-Modules =>>  Anything that we required for our project present inside the node module . When we installed
-//                     any kind of dependcies ( like Bootstrap , React-Router , Axios , Chakra-UI etc) .
+//                     any kind of dependcies ( like Bootstrap, React-Router, Axios, Chakra-UI etc) .
 //                     it will stored inside the node module .  
 
 
-// package.json =>>  whatever dependcies stored inside node module , it  Will Listed in package.json
+// package.json =>>  whatever dependcies stored inside node module, it  Will Listed in package.json
 
 
 // package-lock.json =>> whole Dependcies tree (Whatever installed or unistalled) is present in package-lock.json .
